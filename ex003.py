2ab54844562d57722f75f833b55ca741bfeb4875
jefferson <computadores.rp@gmail.com>
2023-06-06 07:51:16 -0300

    Exercício ex003.py esta terminado.

---
+++ ex003.py
@@ -0,0 +1,10 @@
+# Exercício 003
+
+# Variáveis
+PrimeiroNumero = float(input('Digite um número: '))
+SegundoNumero = float(input('Digite outro número: '))
+Soma = PrimeiroNumero + SegundoNumero
+
+# Execução
+
+print('A soma: {} + {} = {}'.format(PrimeiroNumero, SegundoNumero, Soma))
