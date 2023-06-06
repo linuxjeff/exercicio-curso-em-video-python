3efb67a4b914ae29fd6eacbe4554f1e0a9ec043f
jefferson <computadores.rp@gmail.com>
2023-06-06 09:16:59 -0300

    Exercício ex004.py esta terminado.

---
+++ ex004.py
@@ -0,0 +1,10 @@
+# Execícior 004
+
+# Variáveis
+
+Conteudo = input('Digite um valor: ')
+
+# Execução
+print('Tem somente números? \n Resposta: {}'.format(Conteudo.isnumeric()))
+print('Tem somente letras? \n Resposta: {}'.format(Conteudo.isalpha()))
+print('Tem letras e/ou núemros? \n Resposta: {}'.format(Conteudo.isalnum()))
