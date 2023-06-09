9aad289cbf85e20f239004f3ea31a278e6964a21
jefferson <computadores.rp@gmail.com>
2023-06-09 07:56:57 -0300

    O ex004.py foi motificado para ficar como esperado.

--- ex004.py
+++ ex004.py
@@ -5,6 +5,11 @@
 Conteudo = input('Digite um valor: ')
 
 # Execução
+print('O tipo primitivo deste dado é: {}'.format(type(Conteudo)))
+print('Só tem espaços? \n Resposta: {}'.format(Conteudo.isspace()))
 print('Tem somente números? \n Resposta: {}'.format(Conteudo.isnumeric()))
 print('Tem somente letras? \n Resposta: {}'.format(Conteudo.isalpha()))
 print('Tem letras e/ou núemros? \n Resposta: {}'.format(Conteudo.isalnum()))
+print('Esta somente em maiúsculas? \n Resposta: {}'.format(Conteudo.isupper()))
+print('Esta somente em minúculas? \n Resposta: {}'.format(Conteudo.islower()))
+print('Esta capitalizado? \n Resposta: {}'.format(Conteudo.istitle()))
