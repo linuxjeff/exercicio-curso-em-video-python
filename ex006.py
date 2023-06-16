b61eb95567bcc44e34b5a58b20a1970750821dd7
jefferson <computadores.rp@gmail.com>
2023-06-16 13:27:46 -0300

    Inclusão do ex006.py

---
+++ ex006.py
@@ -0,0 +1,8 @@
+# Variáveis
+Numero = float(input('Digite um número: '))
+Dobro = Numero * 2
+Triplo = Numero * 3
+RaizQuadra = Numero ** (1/2)
+
+# Execução
+print('O dobro de {} é {}\nO triplo de {} é {}\nA raiz quadra de {} e {}'.format(Numero, Dobro, Numero, Triplo, Numero, RaizQuadra))
