a7581b8100e319b6e48805165f8d9f4427735c4e
jefferson <computadores.rp@gmail.com>
2023-06-16 13:51:21 -0300

    Inclusão do ex008.py

---
+++ ex008.py
@@ -0,0 +1,8 @@
+# Variáveis
+
+Metros = float(input('Digite o números de metros: '))
+Cemtimetros = Metros * 100
+Milimetros = Metros * 1000
+
+# Execução
+print('Metros {} para cemtímetros {}\nMetros {} para milímetros {}'.format(Metros, Cemtimetros, Metros, Milimetros))
