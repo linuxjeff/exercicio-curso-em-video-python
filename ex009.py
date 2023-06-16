7d2342c5ae413aad064ee45a19524abea06168de
jefferson <computadores.rp@gmail.com>
2023-06-16 14:19:52 -0300

    Inclusão do ex009.py

---
+++ ex009.py
@@ -0,0 +1,8 @@
+# Variáveis
+Numero = int(input('Digite um número: '))
+
+# Execução
+print('Tabuada do {}\n{} x 0 = {:^5}\n{} x 1 = {:^5}'.format(Numero, Numero, Numero * 0, Numero, Numero * 1))
+print('{} x 3 = {:^5}\n{} x 4 = {:^5}\n{} x 5 = {:^5}'.format(Numero, Numero * 3, Numero, Numero * 4, Numero, Numero * 5))
+print('{} x 6 = {:^5}\n{} x 7 = {:^5}\n{} x 8 = {:^5}'.format(Numero, Numero * 6, Numero, Numero * 7, Numero, Numero * 8))
+print('{} x 9 = {:^5}\n{} x 10 = {:^5}'.format(Numero, Numero * 9, Numero, Numero * 10))
