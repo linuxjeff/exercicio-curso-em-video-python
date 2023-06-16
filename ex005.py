abfb47f7ee89dafb1682888ad070fc37b134a3f8
jefferson <computadores.rp@gmail.com>
2023-06-16 12:59:26 -0300

    Inclusão do ex005.py

---
+++ ex005.py
@@ -0,0 +1,8 @@
+# Variáveis
+Numero = int(input('Digite um número: '))
+NumeroAnterios = Numero - 1
+NumeroPosterior = Numero + 1
+
+# Execução
+
+print('O número anterior a {} é {}\nO número posterior a {} é {}'.format(Numero, NumeroAnterios, Numero, NumeroPosterior))
