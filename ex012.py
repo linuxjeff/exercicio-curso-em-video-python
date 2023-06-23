4cb865395f024c442d89aae1d1b935ab1df1d79f
jefferson <computadores.rp@gmail.com>
2023-06-23 09:04:22 -0300

    Inclusão do ex012.py

---
+++ ex012.py
@@ -0,0 +1,11 @@
+# Programa para monstrar 5 porcento de desconto
+
+# Variáveis
+
+Valor = float(input('Qual o falor do produto?\n>>> '))
+
+Desconto = (Valor / 100) * 95
+
+# Execução
+
+print('O valor do Produto com desconto: R$ {:.2f}'.format(Desconto))
