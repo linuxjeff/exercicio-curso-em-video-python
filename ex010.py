34c00b6cb4157b42dbf464b178c75e25806ad8a8
jefferson <computadores.rp@gmail.com>
2023-06-23 08:09:23 -0300

    Inclusão do ex010.py

---
+++ ex010.py
@@ -0,0 +1,8 @@
+# Programa para converter reias em dolares
+
+# Variáveis
+Reais = float(input('Quantos reais você tem?\n>>> '))
+Dolares = Reais / 3.27
+
+# Execução
+print('Com R$ {:.2f} você compra $ {:.2f}'.format(Reais, Dolares))
