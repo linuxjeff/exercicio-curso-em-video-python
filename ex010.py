1b914a5873dca276d058b822b6ce5defa9dff6f3
jefferson <computadores.rp@gmail.com>
2023-06-26 11:28:51 -0300

    O arquivo ex010.py foi modificado.

--- ex010.py
+++ ex010.py
@@ -1,8 +1,8 @@
 # Programa para converter reias em dolares
 
 # Variáveis
-Reais = float(input('Quantos reais você tem?\n>>> '))
+Reais = float(input('Quantos reais você tem?\n>>> R$'))
 Dolares = Reais / 3.27
 
 # Execução
-print('Com R$ {:.2f} você compra $ {:.2f}'.format(Reais, Dolares))
+print('Com R${:.2f} você compra ${:.2f}'.format(Reais, Dolares))
