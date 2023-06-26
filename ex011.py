caa18b4531d297e59cbc12a9ef8224c4910a89ef
jefferson <computadores.rp@gmail.com>
2023-06-26 12:04:27 -0300

    Modificado o arquivo: ex011.py

--- ex011.py
+++ ex011.py
@@ -1,9 +1,14 @@
 # Progama que verifica quanto de tinta será nescessário para pintar a área de uma parede.
 
 # Variáveis
+
 Altura = float(input('Qual a altura da parede?\n>>> '))
+
 Largura = float(input('Qual a largura da parede?\n>>> '))
+
 Area = Altura * Largura
 
+Calculo = Area / 2
+
 # Execução
-print('Litros de tintas necessários: {:.0f}'.format((Area / 2) + 1))
+print('Litros de tintas necessários: {:.2f}'.format(Calculo))
