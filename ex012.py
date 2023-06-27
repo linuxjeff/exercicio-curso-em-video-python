4bca6eac4bdd36e1d35af195d7d44b765d50a30a
jefferson <computadores.rp@gmail.com>
2023-06-27 14:25:07 -0300

    Modificado o arquivo: ex012.py

--- ex012.py
+++ ex012.py
@@ -6,8 +6,8 @@ Valor = float(input('Qual o valor do produto?\n>>> R$'))
 
 Desconto = float(input('Qual a porcentagem do desconto?\n>>> '))
 
-Calculo = (Valor / 100) * (100 - Desconto)
+Calculo = Valor - (Valor * Desconto / 100)
 
 # Execução
 
-print('O valor do Produto com desconto é: R${:.2f}'.format(Desconto))
+print('O valor do Produto com desconto é: R${:.2f}'.format(Calculo))
