f96e36191060036362749cda5284e2dfc351945a
jefferson <computadores.rp@gmail.com>
2023-06-27 15:11:22 -0300

    Criado o arquivo ex014.py.

---
+++ ex014.py
@@ -0,0 +1,8 @@
+# Programa para converter de celso para fahrenheit
+
+# Variáveis
+Celsius = float(input('Qual a temperatura em celsius: '))
+
+# Execução
+
+print('A temperatura em fahrenheit é: {:.1f}°F'.format((Celsius * 1.8) + 32))
