ec33fb64c34de18b58b937740440b625fc372075
jefferson <computadores.rp@gmail.com>
2023-07-07 18:17:47 -0300

    arquivo ex017.py criado.

---
+++ ex017.py
@@ -0,0 +1,14 @@
+# Programa que calcula a hipotenusa.
+
+# Importação de libs
+from math import hypot
+
+# Variáveis
+
+CatetoOposto = float(input('Qual o cateto oposto?\n>>> '))
+
+CatetoAdjacente = float(input('Qual o cateto adjacente?\n>>> '))
+
+# Execução
+
+print('O triangulo retângulo com o cateto oposto igual a {} e o cateto adjacente igual {} tem a hipotenusa de: {}'.format(CatetoOposto, CatetoAdjacente, hypot(CatetoOposto, CatetoAdjacente)))
