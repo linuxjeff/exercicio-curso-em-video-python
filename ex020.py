21efc253a578085ad6e5c12ba76f243ad2ac0abc
jefferson <computadores.rp@gmail.com>
2023-07-14 19:30:29 -0300

    programa ex020.py finalizado.

--- ex020.py
+++ ex020.py
@@ -1,3 +1,17 @@
 # Programa para sortear ordem de apresentação.
 
 # Importando libs
+from random import sample
+
+# Variáveis
+
+Primeiro = input('Digite o nome do primeiro aluno.\n>>> ')
+
+Segundo = input('Digite o nome do segundo aluno.\n>>> ')
+
+Terceiro = input('Digite o nome do terceiro aluno.\n>>> ')
+
+Quarto = input('Digite o nome do quarto aluno.\n>>> ')
+
+# Execução
+print('Onderm de apresentação: {}'.format(sample([Primeiro, Segundo, Terceiro, Quarto], k=4)))
