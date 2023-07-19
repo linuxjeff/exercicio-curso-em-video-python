4b42bd60ef8f37acf3d3cd3a7173a0ac82d05df9
jefferson <computadores.rp@gmail.com>
2023-07-19 20:00:52 -0300

    Início ex022.py

---
+++ ex022.py
@@ -0,0 +1,13 @@
+# Programa para passar informações sobre o nome
+
+# Variáveis
+
+NomeCompleto = input('Qual o seu nome completo?\n>>> ')
+
+NomeCompleto = NomeCompleto.strip()
+
+NomeCompleto = NomeCompleto.split()
+
+NomeCompleto = ''.join(fractions)
+
+print(lon.NomeCompleto())
