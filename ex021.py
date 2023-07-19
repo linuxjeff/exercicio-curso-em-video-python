ecd7b59059f5f4f2860cde1ffba4b1f95da0a56e
jefferson <computadores.rp@gmail.com>
2023-07-19 19:09:50 -0300

    Salvando alterações feitas no ex021.py.

--- ex021.py
+++ ex021.py
@@ -1,5 +1,8 @@
 # Programa para tocar mp3
 
-from playsound import playsound
-
-playsound('11-Three_Years_Later_ft_Emily_Ridgway_.mp3')
+import pygame
+pygame.init()
+pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.play()
+pygame.time.delay(60000)
+pygame.event.wait()
