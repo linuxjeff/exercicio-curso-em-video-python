c5a3c671d65bb00a3305a73b03bd37901ca3d50f
jefferson <computadores.rp@gmail.com>
2023-07-24 19:29:22 -0300

    Início o ex023.py.

--- ex023.py
+++ ex023.py
@@ -1,10 +1,6 @@
 # O Programa lê um número de 0 a 9999 e retorna a quantidade em cada casa.
 
-Genises = '0000'
-
 Numero = input('Ditite um numero de 0 a 9999\n>>> ').strip()
-
-Unidade = Numero
 
 print('Unidade: {}'.format(Numero[3]))
 print('Dezena: {}'.format(Numero[2]))
