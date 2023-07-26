bbe699a725b09a0f8a738a8886b87a4606c6cd74
jefferson <computadores.rp@gmail.com>
2023-07-26 19:40:28 -0300

    Corrigido o bug no ex023.py.

--- ex023.py
+++ ex023.py
@@ -1,8 +1,13 @@
 # O Programa lê um número de 0 a 9999 e retorna a quantidade em cada casa.
 
-Numero = input('Ditite um numero de 0 a 9999\n>>> ').strip()
+Numero = int(input('Ditite um numero de 0 a 9999\n>>> ').strip())
 
-print('Unidade: {}'.format(Numero[3]))
-print('Dezena: {}'.format(Numero[2]))
-print('Centena: {}'.format(Numero[1]))
-print('Milhar: {}'.format(Numero[0]))
+Unidade = Numero // 1 % 10
+Dezena = Numero // 10 % 10
+Centena = Numero // 100 % 10
+Milhar = Numero // 1000 % 10
+
+print('Unidade: {}'.format(Unidade))
+print('Dezena: {}'.format(Dezena))
+print('Centena: {}'.format(Centena))
+print('Milhar: {}'.format(Milhar))
