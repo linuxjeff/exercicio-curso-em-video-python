c9844774635a24540f3fe1d79b745a1729f938c8
jefferson <computadores.rp@gmail.com>
2023-07-26 18:22:09 -0300

    Finalizado ex026.py.

--- ex026.py
+++ ex026.py
@@ -1,5 +1,7 @@
-# Informa quantos a a no texto
+# Informa quantos letras a tem no texto
 
 LetrasA = input('Digite uma frase.\n>>> ').lower().strip()
 
-print(LetrasA)
+print('O número de letras a é: {}'.format(LetrasA.count('a')))
+print('O primeiro a esta na posição: {}'.format(LetrasA.find('a')))
+print('O ultimo a esta na posição: {}'.format(LetrasA.rfind('a')))
