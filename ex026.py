2a7c2a32415e57ab46f269c88e60e77d86e4785f
jefferson <computadores.rp@gmail.com>
2023-07-29 17:45:47 -0300

    Erro de português em ex025.py.

--- ex026.py
+++ ex026.py
@@ -3,5 +3,5 @@
 LetrasA = input('Digite uma frase.\n>>> ').lower().strip()
 
 print('O número de letras a é: {}'.format(LetrasA.count('a')))
-print('O primeiro a esta na posição: {}'.format(LetrasA.find('a')))
-print('O ultimo a esta na posição: {}'.format(LetrasA.rfind('a')))
+print('O primeiro a esta na posição: {}'.format(LetrasA.find('a')+1))
+print('O ultimo a esta na posição: {}'.format(LetrasA.rfind('a')+1))
