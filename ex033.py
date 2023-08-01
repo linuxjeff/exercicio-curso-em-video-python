1bd76545ce24a755acf1ff57f1575042d656be0f
jefferson <computadores.rp@gmail.com>
2023-08-01 13:45:21 -0300

    Iniciado o ex033.py

---
+++ ex033.py
@@ -0,0 +1,12 @@
+# Recebe trẽs números e diz qual é o maior e qual é o menor.
+
+Primeiro = int(input('Digite o primeiro número.\n>>> '))
+
+Segundo = int(input('Digite o segundo número.\n>>> '))
+
+Terceiro = int(input('Digite o terceiro número.\n>>> '))
+
+print(Primeiro, Segundo, Terceiro)
+
+if Primeiro < Segundo:
+    if P
