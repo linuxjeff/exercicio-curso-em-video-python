d9b6f3f0b63f12dc33a3305bc209768b22abc4fb
jefferson <computadores.rp@gmail.com>
2023-08-04 19:13:29 -0300

    finalizado o ex033.py.

--- ex033.py
+++ ex033.py
@@ -8,5 +8,29 @@ Terceiro = int(input('Digite o terceiro número.\n>>> '))
 
 print(Primeiro, Segundo, Terceiro)
 
+# Menor
 if Primeiro < Segundo:
-    if P
+    if Primeiro < Terceiro:
+        Menor = Primeiro
+    else:
+        Menor = Terceiro
+else:
+    if Segundo < Terceiro:
+        Menor = Segundo
+    else:
+        Menor = Terceiro
+
+# Maior
+if Primeiro > Segundo:
+    if Primeiro > Terceiro:
+        Maior = Primeiro
+    else:
+        Maior = Terceiro
+else:
+    if Segundo > Terceiro:
+        Maior = Segundo
+    else:
+        Maior = Terceiro
+
+print('O menor número é: {}'.format(Menor))
+print('O Maior número é: {}'.format(Maior))
