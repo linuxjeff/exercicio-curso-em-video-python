b1369b3a447ad2841884ab90e977c400170081fa
jefferson <computadores.rp@gmail.com>
2023-08-07 18:28:09 -0300

    ex029.py Modificado

--- ex029.py
+++ ex029.py
@@ -3,6 +3,6 @@
 Velocidade = int(input('Qual a velocidade do veículo?\n>>> '))
 
 if Velocidade > 80:
-    print('Você ultrapassou o limite de velocidade.\nSua multa tel o valor de: {:.2f}'.format((Velocidade - 80)*7))
+    print('Você ultrapassou o limite de velocidade.\nSua multa tel o valor de: R${:.2f}'.format((Velocidade - 80)*7))
 else:
     print('Você é um condutor responsável!')
