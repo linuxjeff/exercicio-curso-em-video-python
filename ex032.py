467e8756597c4575a288bd4cd2cfe84387e13b40
jefferson <computadores.rp@gmail.com>
2023-08-07 19:40:25 -0300

    ex032.py Modificado

--- ex032.py
+++ ex032.py
@@ -1,10 +1,14 @@
 # Indica se o ano é bissexto
 
 from calendar import isleap
+from datetime import date
 
-Ano = int(input('Digite um ano.\n>>> '))
+Ano = int(input('Digite um ano, digite 0 para analizar o ano atual.\n>>> '))
+
+if Ano == 0:
+    Ano = date.today().year
 
 if str(isleap(Ano)) == 'True':
-    print('O ano é bissexto!')
+    print('O ano {} é bissexto!'.format(Ano))
 else:
-    print('O ano não é bissexto!')
+    print('O ano {} não é bissexto!'.format(Ano))
