6dc9f25abfbea236186c870c6a0ea90ee520f466
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-08-26 15:35:20 -0300

    - Retirado debug de variávei e finalizado versão.

--- ex036.py
+++ ex036.py
@@ -6,6 +6,7 @@
 #   - termidado o if para texto de aprovação.
 #   - Corrigido erro do if. o printe não estava com variável.
 #   - Corrigido erro do printe.
+#   - Retirado debug de variávei e finalizado versão.
 
 # Variáveis
 
@@ -23,7 +24,3 @@ if ValorParcelas < ValorMaxDaParcelas:
     print('Seu empréstimo foi aprovado e a parcelas tem o valor de: R${:.2f}'.format(ValorParcelas))
 else:
     print('O empréstimo não foi aprovado, o valor da parcela excede 30 porcento da sua renda mensal.')
-
-# Debug variáveis
-
-print(CasaValor, SalarioDoComprador, AnosParaPagar, ValorParcelas, ValorMaxDaParcelas)
