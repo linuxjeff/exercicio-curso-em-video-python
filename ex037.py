787bde79d6c3962f61c1ddf269ab7505186e5b5c
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-08-26 16:02:06 -0300

    Foi iniciado a versão 0.0.1 do convert-py.

---
+++ ex037.py
@@ -0,0 +1,21 @@
+# convert-py - Programa convert números decimal para outras bases
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa converte números decimais para binário, hexedecimal e
+#              octal
+#
+#   Exemplos:
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 26-08-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
