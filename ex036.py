c25b95f6b09d212ba2642959f90b6eab7b08ff80
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-08-26 15:29:32 -0300

    Corrigido erro do if. o printe não estava com variável.

--- ex036.py
+++ ex036.py
@@ -4,6 +4,7 @@
 #   - início
 # v0.0.2
 #   - termidado o if para texto de aprovação.
+#   - Corrigido erro do if. o printe não estava com variável.
 
 # Variáveis
 
@@ -18,7 +19,7 @@ ValorParcelas = CasaValor / (AnosParaPagar * 12)
 ValorMaxDaParcelas = SalarioDoComprador / 100 * 30
 
 if ValorParcelas < ValorMaxDaParcelas:
-    print('Seu empréstimo foi aprovado e a parcelas tem o valor de: R${:.f2}')
+    print('Seu empréstimo foi aprovado e a parcelas tem o valor de: R${:.f2}'.format(ValorParcelas))
 else:
     print('O empréstimo não foi aprovado, o valor da parcela excede 30 porcento da sua renda mensal.')
 
