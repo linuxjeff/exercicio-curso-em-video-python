602ba477d043e41d167f90870a5f1ef49c421575
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-08-26 16:12:57 -0300

    Consertado erro de espaço na variável NumeroD. Colocado local mais apropiado para a escolha do número na variável Base.

--- ex037.py
+++ ex037.py
@@ -16,16 +16,18 @@
 #
 #  v0.0.1 26-08-2023, Jefferson Santana
 #   - Versão inicial
-#   - Criada as varáveis e o debug de variávei.
+#   - Criada as varáveis e o debug de variáveis.
+#   - Consertado erro de espaço na variável NumeroD
+#   - Colocado local mais apropiado para a escolha do número na variável Base.
 #
 # Licença: MIT.
 #
 
 # Variáveis
 
-NumeroD = int(input('Digite um número.\n>>>'))
+NumeroD = int(input('Digite um número.\n>>> '))
 
-Base = int(input('Para qual base quer converter seu número?\n1 - Binário\n2 - Hexedecimal\n3 - Octal'))
+Base = int(input('Para qual base quer converter seu número?\n1 - Binário\n2 - Hexedecimal\n3 - Octal\n>>> '))
 
 # Debug das variávei
 print(NumeroD, Base)
