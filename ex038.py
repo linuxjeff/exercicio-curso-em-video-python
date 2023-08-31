4d0214526791ba1406748a6017f035a0ad9b59aa
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-08-31 19:55:26 -0300

    Criado o cabeçalho do programa.

---
+++ ex038.py
@@ -0,0 +1,19 @@
+# nome_completo - O programa lê dois valores e os comparam.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa compara os números e mostra uma mensagem dizendo qual
+#              é o maior o se são iguais.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 31-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
