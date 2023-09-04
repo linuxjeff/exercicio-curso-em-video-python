4514a653dc7f06849372e41b6790872c7cb020cf
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-04 19:59:42 -0300

    Foi criado o cabeçalho do ex039.py.

---
+++ ex039.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# ex039 - Programa para verificar o andamento do alistamento.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa para informar quando deve efetuar o alistamo nas forças
+#              armadas.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 04-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
