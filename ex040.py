b05f858f3d041d886af265c4c34c8e483245c3a4
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-07 10:22:28 -0300

    Criado o cabeçalho do solfware.

---
+++ ex040.py
@@ -0,0 +1,19 @@
+# ex040 - Faz cálculo de média.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe duas notas do aluno e faz o cálculo da média. Mostra se
+#              o aluno foi aprovado, reprovado ou ficou de recuperação.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 07-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
