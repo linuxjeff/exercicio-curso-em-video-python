4b76f9957158ea4669cc7c5153f143b670782c10
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-13 19:59:35 -0300

    Criado o cabeçalho do programa

---
+++ ex042.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# ex042 - Diz se trẽs retas formam um triângulo.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa pede três retas e verifica e estas retas conseguem
+#              forma um triângulo e qual tipo de triângulo forma.
+#
+#   Exemplos:
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 13-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
