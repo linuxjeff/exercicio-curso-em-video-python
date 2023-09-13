6ded1cef561cc36ed2b763d9139c3776a2b7800f
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-13 19:14:11 -0300

    Criado o cabeçalho.

---
+++ ex041.py
@@ -0,0 +1,18 @@
+# ex041 - Indica a categoria do atleta.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Vai mostrar a qual categoria de natação que o atleta pertence.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 13-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
