5bdbd6ff5e46a8ae8311647aad734d5b408e3b50
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-13 19:47:20 -0300

    - Criado o IF para testar as idades e passar a categoria - Retirada área de debug - Criada área de exemplo

--- ex041.py
+++ ex041.py
@@ -3,6 +3,11 @@
 # Site      : https://github.com/linuxjeff
 # Autor     : Jefferson Santana <computadores.rp@gmail.com>
 #
+# Exemplo:
+# Qual seu ano de nascimento?
+# >>> 2007
+# Você é da categoria: junior
+#
 # -----------------------------------------------------------------------------
 #   Descrição: Vai mostrar a qual categoria de natação que o atleta pertence.
 #
@@ -18,6 +23,9 @@
 #   - Criada área de importação de bibliotecas
 #   - importada a biblioteca datetime
 #   - Criada a variável "Idade"
+#   - Criado o IF para testar as idades e passar a categoria
+#   - Retirada área de debug
+#   - Criada área de exemplo
 #
 # Licença: MIT.
 #
@@ -34,8 +42,17 @@ AnoDeNascimento = int(input('Qual seu ano de nascimento?\n>>> '))
 Idade = date.today().year - AnoDeNascimento
 
 # -----------------------------------------------------------------------------
-# Debug de variáveis
+# Execução Direta
 
-print(AnoDeNascimento, Idade)
+if Idade <= 9:
+    print('Você é da categoria: mirim')
+elif Idade <= 14:
+    print('Você é da categoria: infantil')
+elif Idade <= 19:
+    print('Você é da categoria: junior')
+elif Idade <= 20:
+    print('Você é da categoria: sênior')
+else:
+    print('Você é da categoria: Master')
 
 # -----------------------------------------------------------------------------
