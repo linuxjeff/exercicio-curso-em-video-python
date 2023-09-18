49109bc12e761ff4d8e29f30a85b4cdd5a43f1fe
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-18 13:34:26 -0300

    Criado o cabeçalho.

---
+++ ex043.py
@@ -0,0 +1,21 @@
+# nome_completo - Programa para calcular o IMC.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Este programa cálcula o IMC de um indivíduo e diz em qual faixa
+#              ele está de peso.
+#
+#   Exemplos:
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 18-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
