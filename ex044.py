4fb3b99d62ecd13af37b85c8dabec571551022b8
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-21 13:22:56 -0300

    Criado o cabeçalho do programa.

---
+++ ex044.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#
+# ex044 - Programa aplica desconto em valor de compra.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa aplica o desconto ao produto dependendo a forma de
+#              pagamento. O programa aceita pagamento em dinheiro, cheque
+#              (á vista), cartão crédito e cartão de débito.
+#
+#   Exemplos:
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 21-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
