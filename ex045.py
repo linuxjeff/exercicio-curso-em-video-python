a5b8e05ca314ee7a58285a9a76ca5410ca26e337
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-09-25 18:27:29 -0300

    Criado o cabeçalho do programa.

---
+++ ex045.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#
+# ex045 - O jogo de jokenpô
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O jogador escolhe entre peda papel ou tesousa. Depois o
+#              CPU escolhe e no final vemos quem ganhou.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 25-09-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
