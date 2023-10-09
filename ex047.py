a7573778186b31b00f3b875b36aca3adbb3d9bdd
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-09 18:53:48 -0300

    Criado cabeçalho.

---
+++ ex047.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+# ex047 - Contador de pares.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Conta todos os número pares de um a cinquenta.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 09-10-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
