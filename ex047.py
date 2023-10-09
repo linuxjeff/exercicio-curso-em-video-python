d03294ccf7deb452eb4fa77d660f513bfa665382
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-09 19:03:45 -0300

    - Versão inicial - importada biblioteca time - criado o for para contar números pares

--- ex047.py
+++ ex047.py
@@ -15,6 +15,21 @@
 #
 #  v0.0.1 09-10-2023, Jefferson Santana
 #   - Versão inicial
+#   - importada biblioteca time
+#   - criado o for para contar números pares
 #
 # Licença: MIT.
 #
+# -----------------------------------------------------------------------------
+# importando bibliotecas
+
+from time import sleep
+
+# -----------------------------------------------------------------------------
+# Execução
+
+for c in range(2, 51, 2):
+    print(c)
+    sleep(0.5)
+
+# -----------------------------------------------------------------------------
