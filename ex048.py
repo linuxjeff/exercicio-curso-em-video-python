6ce1da159ee9d02d76e37d5e364ddf4c6b6a174c
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-09 20:06:39 -0300

    - Corrigido erro no cálculo, falta ver se o número era impar

--- ex048.py
+++ ex048.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 #
-# ex048 - Soma números mutiplos de três
+# ex048 - Soma números mutiplos de três e impar
 #
 # Site      : https://github.com/linuxjeff
 # Autor     : Jefferson Santana <computadores.rp@gmail.com>
 #
 # -----------------------------------------------------------------------------
-#   Descrição: Soma os números multiplos de três de um a quinhentos.
+#   Descrição: Soma os números multiplos de três de um a quinhentos que são impar.
 #
 # -----------------------------------------------------------------------------
 #
@@ -18,6 +18,7 @@
 #   - criado cabeçalho
 #   - Criado for para somar mutiplos
 #   - Criado print para mostrar resultado
+#   - Corrigido erro no cálculo, falta ver se o número era impar
 #
 # Licença: MIT.
 #
@@ -25,6 +26,7 @@
 mutiplos = 0
 
 for contador in range(1, 501):
-    if c % 3 == 0:
-        mutiplos = mutiplos + contador
+    if contador % 3 == 0:
+        if contador % 2 == 1:
+            mutiplos = mutiplos + contador
 print('A soma dos multiplos é: {}'.format(mutiplos))
