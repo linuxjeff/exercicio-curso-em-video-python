084ade6d907d85a29eb8c858d6fb84a33d985f5a
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-12 07:53:37 -0300

    Criado o cabeçalho do programa.

---
+++ ex049.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+# ex049 - Mostra a tabuada de um número
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Ele vai mostrar a tabuada de qualquer número de zero a dez
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 12-10-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
