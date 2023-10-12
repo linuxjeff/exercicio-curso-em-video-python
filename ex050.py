f5a82c5bf5c08a07511ec7b2efa099ae6e646768
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-12 08:44:54 -0300

    - Versão inicial - Criado cabeçalho - Criada área de variáveis principais - Criado for para somar nímeros pares - Criado print para mostrar soma final.

---
+++ ex050.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+#
+# ex050 - Mostra a soma dos números pares.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa lê seis números e soma somente os pares.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 12-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho
+#   - Criada área de variáveis principais
+#   - Criado for para somar nímeros pares
+#   - Criado print para mostrar soma final.
+#
+# Licença: MIT.
+#
+
+# variáveis principais
+
+Soma = 0
+
+# For para somar números.
+
+for Contador in range(1, 7):
+    Numero = int(input('Digite um número.\n>>> '))
+    if Numero % 2 == 0:
+        Soma = Soma + Numero
+
+print('Soma dos valores pares digitados: {}'.format(Soma))
