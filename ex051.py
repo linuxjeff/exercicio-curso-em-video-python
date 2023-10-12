eda3f74fba51875c2788a33b7e630f057b41c1f9
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-12 09:06:36 -0300

    - Criado o cabeçalho do programa

---
+++ ex051.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+# ex051 - cálcula a PA.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe o primeiro termo da PA e sua razão e mostra os próximos
+#              dez termos da PA.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 12-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
