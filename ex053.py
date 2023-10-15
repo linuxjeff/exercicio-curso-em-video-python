e791dfaaf31d4dfe812e234afbebd44f0a9a8aac
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-15 09:25:10 -0300

    - Versão inicial - criado o cabeçalho

---
+++ ex053.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex053 - Verifica se a frase é um palíndromo.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Este programa recebe uma frase e verificar se ela é um
+#              palíndromo.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 15-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - criado o cabeçalho
+#
+# Licença: MIT.
+#
