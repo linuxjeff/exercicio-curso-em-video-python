cfb8f6cb37a9cbf77cd7d0ecf74cc1f3cdf0ba2b
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-15 08:02:05 -0300

    - Criada a variável para receber primeiro termo - Criada a variável para receber a razão - Criado o for para gerar oito termos da PA - Criado os prints para mostrar os resultados

--- ex051.py
+++ ex051.py
@@ -15,6 +15,28 @@
 #  v0.0.1 12-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a variável para receber primeiro termo
+#   - Criada a variável para receber a razão
+#   - Criado o for para gerar oito termos da PA
+#   - Criado os prints para mostrar os resultados
 #
 # Licença: MIT.
 #
+
+# Variáveis principais
+
+PrimeiroTermo = int(input('Digite o a1 da PA.\n>>> '))
+
+Razao = int(input('Digite o r da PA.\n>>> '))
+
+# Primeiro termo
+
+print('({}'.format(PrimeiroTermo),end=',')
+
+# For para gerar a PA.
+
+for Contador in range(1, 9):
+    PrimeiroTermo = PrimeiroTermo + Razao
+    print('{}'.format(PrimeiroTermo),end=',')
+
+print('{})'.format(PrimeiroTermo + Razao))
