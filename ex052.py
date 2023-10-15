60cb1952e1a46d3d2f0e34e8364c015faf18d924
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-15 08:17:45 -0300

    Criado o cabeçalho.

---
+++ ex052.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#
+# ex052 - Fala se um número é primo
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe um número e informa se ele é primo ou não é primo.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 15-10-2023, Jefferson Santana
+#   - Versão inicial
+#
+# Licença: MIT.
+#
