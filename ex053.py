7a65017127a8a018949120709e8f41a9f17d1bbf
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-16 18:59:22 -0300

    - Criada área de variáveis - Criado o FOR para inveter frese - Cirado o IF para verificar se a frase é um palíndromo

--- ex053.py
+++ ex053.py
@@ -16,7 +16,33 @@
 #
 #  v0.0.1 15-10-2023, Jefferson Santana
 #   - Versão inicial
-#   - criado o cabeçalho
+#   - Criado o cabeçalho
+#   - Criada área de variáveis
+#   - Criado o FOR para inveter frese
+#   - Cirado o IF para verificar se a frase é um palíndromo
 #
 # Licença: MIT.
 #
+
+# Variáveis
+
+TrocaDeCara = str.maketrans({' ': '', '.': '', '!': '', '?': '', ',': ''})
+
+Frase = input('Digite uma frase.\n>>> ').lower().strip().lstrip().translate(TrocaDeCara)
+
+Valor = len(Frase)
+
+ContagemDeCaracteres = Valor - 1
+
+Esarf = ''
+
+# For para inveter frese.
+for c in range(0, Valor):
+    Esarf = Esarf + Frase[ContagemDeCaracteres]
+    ContagemDeCaracteres = ContagemDeCaracteres - 1
+
+# If para verificar se a frase é um palíndromo.
+if Frase == Esarf:
+    print('A frase digitada é um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo.')
