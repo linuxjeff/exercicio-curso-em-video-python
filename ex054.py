ead0e3408163902f37d4bdcf59349914c65aeabc
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-16 19:14:01 -0300

    - Versão inicial - Criado cabeçalho.

---
+++ ex054.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex054 - Mostra quem já compretou a maior idade.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe sete anos de nacimento e cálcula quais anos já passaram
+#              da maior idade.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 16-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho.
+#
+# Licença: MIT.
+#
