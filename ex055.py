d2c9dff56970e6ae1277c0ba03f5c57694839151
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-19 19:00:41 -0300

    - Criado o cabeçalho

---
+++ ex055.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex055 - Mostra o maior e o menor peso.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe o peso de cinco pessoas e mostra qual é o maior e qual é
+#              o menor.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 19-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho
+#
+# Licença: MIT.
+#
