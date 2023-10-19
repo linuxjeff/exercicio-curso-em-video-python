2ac633da765272617ede4e16d52df962d6dceea4
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-19 19:20:46 -0300

    - Criado for para receber e testar qual é o maior e o menor peso - Criado print para mostrar o resultado do for

--- ex055.py
+++ ex055.py
@@ -17,6 +17,25 @@
 #  v0.0.1 19-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho
+#   - Criado for para receber e testar qual é o maior e o menor peso
+#   - Criado print para mostrar o resultado do for
 #
 # Licença: MIT.
 #
+
+# Variáveis
+
+MaiorPeso = float(0)
+
+MenorPeso = float(0)
+
+# For para receber e testar os pesos.
+for pessoas in range(1, 6):
+    Peso = float(input('Digite o seu peso.\n>>> '))
+    if Peso > MaiorPeso:
+        MaiorPeso = Peso
+    elif MenorPeso == 0 or Peso < MenorPeso:
+        MenorPeso = Peso
+
+# Print que mostra o resultado do for.
+print('O Menor peso é: {:.2f}Kg\nO Maior peso é: {:.2f}Kg'.format(MenorPeso, MaiorPeso))
