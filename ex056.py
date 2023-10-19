db89642bce090fb6a1c9d9d5c7ba5838ce617a48
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-19 19:39:44 -0300

    - Versão inicial - Criado o cabeçalho

---
+++ ex056.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#
+# ex056 - Recebe nome, idade e sexo.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe nome, idade e sexo de quatro pessoas. Fala a média de
+#              idade, o homem mais velo e e quantas mulheres tem menos de
+#              vinte.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 19-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho
+#
+# Licença: MIT.
+#
