566e3501b4b9f3cd09d98013a6afd5924ea92873
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-22 08:48:54 -0300

    - Criada área de variáveis - Criado for para receber cadastro das pessoas e filtrar os dados. - Criados prints para escrever o resultados na tela.

--- ex056.py
+++ ex056.py
@@ -18,12 +18,38 @@
 #  v0.0.1 19-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho
+#   - Criada área de variáveis
+#   - Criado for para receber cadastro das pessoas e filtrar os dados.
+#   - Criados prints para escrever o resultados na tela.
 #
 # Licença: MIT.
 #
 
-Garanta = 0
+# Variáveis
+Media = 0
 
+HomemVelho = ''
+
+IdadeVelho = 0
+
+MulheresMenorVinte = 0
+####
+
+# For para receber e testar pessoas cadastradas.
 for pessoas in range(1, 5):
-    Nome = str(input('Qual seu Nome?\n>>> ')).lstrip().rstrip().title(), int(input('idade'))
-    print(Nome[1])
+    Nome = str(input('Qual seu Nome?\n>>> ')).lstrip().rstrip().title()
+    Idade = int(input('Qual a sua idede?\n>>> '))
+    Sexo = str(input('Qual seu sexo?\nDigite H (homem) e M (mulher).\n>>> ')).lstrip().rstrip().upper()
+    Media = Media + Idade
+    if Sexo == 'H' and Idade > IdadeVelho:
+        IdadeVelho = Idade
+        HomemVelho = Nome
+    if Sexo == 'M' and Idade < 20:
+        MulheresMenorVinte = MulheresMenorVinte + 1
+###
+
+# Escrevendo resultados na tela.
+print('A média de idade das quatro pessoas é de: {:.0f}'.format(Media/4))
+print('O homem mais velho é: {}'.format(HomemVelho))
+print('O número de mulheres menores de 20 anos é de: {}'.format(MulheresMenorVinte))
+###
