e87f1de86a1aedc9af44acb7c9241bd5a639e932
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-26 20:00:32 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex058.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex052 - Jogo de adivinhação.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: A CPU sorteia um número e o jogador tem que adivinhar qual numero
+#   que foi.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 26-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
