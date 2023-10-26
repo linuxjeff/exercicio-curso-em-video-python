d2fc0868f48d3002fe88aa0d4986b4cc96374069
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-26 19:52:26 -0300

    - Criado while para testar a variável Sexo

--- ex057.py
+++ ex057.py
@@ -16,6 +16,24 @@
 #  v0.0.1 26-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho
+#   - Criado while para testar a variável Sexo
 #
 # Licença: MIT.
 #
+
+# Variáveis de início.
+
+Sexo = ''
+
+###
+
+# While para testar "Sexo".
+while Sexo != 'M' and Sexo != 'F':
+    Sexo = str(input('Digite seu sexo. [M para masculino e F para feminino]\n>>> ')).upper()
+    if Sexo != 'M' and Sexo != 'F':
+        print('Opção invalida!\nTente novamente.')
+    else:
+        if Sexo == 'M':
+            print('Você é do sexo masculino.')
+        else:
+            print('Você é do sexo feminino.')
