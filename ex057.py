107b1de564d73fa4653cdae51e7cabc6d8587adc
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-26 07:20:38 -0300

    - Versão inicial - Criado o cabeçalho

---
+++ ex057.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#
+# ex057 - Recebe o sexo da pessoa.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe o sexo da pessoa e termina qunado a resposta for correta.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 26-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho
+#
+# Licença: MIT.
+#
