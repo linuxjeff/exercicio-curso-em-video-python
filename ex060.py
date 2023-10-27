800600a291aac0c5bc523ca262a888231bacb175
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-27 13:50:16 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex060.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#
+# ex060 - Cálcula o fatorial.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe um número e cálcula o seu fatorial.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 27-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
