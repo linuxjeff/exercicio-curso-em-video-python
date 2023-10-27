1462313a9e3ce3ffcd25f4b19bb2d4bcb826a4a2
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-27 13:44:06 -0300

    - Criada área para variáveis iníciais - Criado while para o teste - Criado o if para operação com números - Criado o if para ver qual número é maior

--- ex059.py
+++ ex059.py
@@ -17,6 +17,46 @@
 #  v0.0.1 27-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada área para variáveis iníciais
+#   - Criado while para o teste
+#   - Criado o if para operação com números
+#   - Criado o if para ver qual número é maior
 #
 # Licença: MIT.
 #
+
+# Variáveis iníciais
+
+NumeroUm = float(input('Digite um número.\n>>> '))
+
+NumeroDois = float(input('Digite outro número.\n>>> '))
+
+Opcao = 0
+
+###
+
+# while para testar os números recebidos.
+while Opcao != 5:
+    # Variável que recebe a opção do while.
+    Opcao = int(input('Digite o número da opção escolhida. ↓\n'
+                      '1 - Somar\n2 - multiplicar\n3 - Maior\n'
+                      '4 - Novos números\n5 - Sair\n>>> '))
+    # If que faz os testes dos números.
+    if Opcao == 1:
+        print('A soma de {} com {} é {}'.format(NumeroUm, NumeroDois, NumeroUm+NumeroDois))
+    elif Opcao == 2:
+        print('{} mutiplicado por {} é igual a {}'.format(NumeroUm, NumeroDois, NumeroUm * NumeroDois))
+    elif Opcao == 3:
+        # if para testar qual é o maior número.
+        if NumeroUm == NumeroDois:
+            print('O número {} é igual ao número {}'.format(NumeroUm, NumeroDois))
+        elif NumeroUm > NumeroDois:
+            print('O número {} é maior que o {}'.format(NumeroUm, NumeroDois))
+        else:
+            print('O número {} é maior que o número {}'.format(NumeroDois, NumeroUm))
+    elif Opcao == 4:
+        NumeroUm = float(input('Digite um número.\n>>> '))
+
+        NumeroDois = float(input('Digite outro número.\n>>> '))
+    else:
+        print('Até a próxima! :)')
