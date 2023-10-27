17c8383144562f3257515c79c2eb3ab944282faa
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-27 12:54:23 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex059.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex059 - Uma calculadora
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe dois número e realiza uma operação matemática
+#   escolhida.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 27-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
