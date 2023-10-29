54f7d42f8e06912d9b3e2d259f36055b8d5990e0
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-29 11:12:48 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex061.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#
+# ex061 - Calcula uma PA
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Progragrama calcula os dez primeiros termos de uma PA.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 29-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
