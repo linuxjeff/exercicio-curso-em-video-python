3aa4b76b3c89eae4fe8a3732cd8511516ebfaccd
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-29 11:40:47 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex062.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex062 - Mostrar uma PA com termos indefinodos.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Mostra os termos de uma PA e pergunta se deseja ver mais
+#   termos.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 29-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
