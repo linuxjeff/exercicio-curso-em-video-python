9dd895fb5cc8193901ab6e01979e6b99f04c1442
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-29 11:33:31 -0300

    - Criada área de variáveis íniciais - Criado print do ínicio da PA - Criado while para calcular o meio da PA - Criado print para mostrar o fim da PA

--- ex061.py
+++ ex061.py
@@ -16,6 +16,32 @@
 #  v0.0.1 29-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada área de variáveis íniciais
+#   - Criado print do ínicio da PA
+#   - Criado while para calcular o meio da PA
+#   - Criado print para mostrar o fim da PA
 #
 # Licença: MIT.
 #
+
+# Variáveis íniciais
+
+Contador = 0
+
+PrimeiroTermo = int(input('Digite o primeiro termo da PA.\n>>> '))
+
+Razao = int(input('Digite a razão da PA.\n>>> '))
+
+###
+# Printe do ínicio da PA.
+print('PA: ({}, '.format(PrimeiroTermo), end='')
+###
+# While para calcular oito termos da PA.
+while Contador != 8:
+    PrimeiroTermo += Razao
+    print('{}, '.format(PrimeiroTermo), end='')
+    Contador += 1
+###
+# Printe do último termo da PA.
+print('{})'.format(PrimeiroTermo + Razao))
+###
