3d117387dc83909e16c7b5a04042e698c99f26e4
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-29 10:36:10 -0300

    - Criada área de variáveis iniciais - Criado while para calcular o Fatorial - Criado o printe para mostrar o resultado

--- ex060.py
+++ ex060.py
@@ -16,6 +16,28 @@
 #  v0.0.1 27-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada área de variáveis iniciais
+#   - Criado while para calcular o Fatorial
+#   - Criado o printe para mostrar o resultado
 #
 # Licença: MIT.
 #
+
+# Variáveis iniciais
+
+Numero = int(input('Ditigte um numero para calcular o seu fatorial.\n>>> '))
+
+Fatorial = Numero
+
+RNumero = Numero
+
+###
+
+# While para calcular o fatorial.
+while Numero != 1:
+    Numero = Numero - 1
+    Fatorial = Fatorial * Numero
+###
+# Printe do resultado
+print('O fatorial de {} é: {}'.format(RNumero, Fatorial))
+###
