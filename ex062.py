4682a6172c5b227c4760d966e3d41ecbb060cbaa
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-30 07:21:48 -0300

    - Criado while para receber opção

--- ex062.py
+++ ex062.py
@@ -17,6 +17,50 @@
 #  v0.0.1 29-10-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criado while para receber opção
 #
 # Licença: MIT.
 #
+
+# Variáveis íniciais
+
+Contador = 0
+
+PrimeiroTermo = int(input('Digite o primeiro termo da PA.\n>>> '))
+
+Razao = int(input('Digite a razão da PA.\n>>> '))
+
+Opcao = 0
+
+OTermo = 0
+
+Rotdatnoc = 8
+
+###
+while Opcao != 2:
+    OTermo = PrimeiroTermo
+    Contador = 0
+    Opcao = 0
+    # Printe do ínicio da PA.
+    print('PA: ({}, '.format(OTermo), end='')
+    ###
+    # While para calcular oito termos da PA.
+    while Contador != Rotdatnoc:
+        OTermo += Razao
+        print('{}, '.format(OTermo), end='')
+        Contador += 1
+    ###
+    # Printe do último termo da PA.
+    print('{})'.format(OTermo + Razao))
+    ###
+    # Este while recebe a opção e o número de termos a ser mostrados.
+    while Opcao != 1 and Opcao != 2:
+        Opcao = int(input('Deseja ver mais termos da PA?\nO padrão é dez termos.\n1 - Sim\n2 - Não\n>>> '))
+        if Opcao != 1 and Opcao != 2:
+            print('Opção invalida. Tente novamente.')
+        elif Opcao == 1:
+            Rotdatnoc = int(input('Quantos termos deseja ver da PA?\n>>> '))
+            if Rotdatnoc <= 10:
+                Rotdatnoc = 8
+            Rotdatnoc = Rotdatnoc - 2
+    ###
