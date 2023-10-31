5fc0dd3812854e6303f19f4e79fc49b6af77afaf
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-10-31 07:22:57 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex063.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex063 - Mostra a sequência de Fibonacci.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe n interiro e mostra na tela os nprimeiros elementos de
+#   uma sequência de Fibonacci.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 31-10-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
