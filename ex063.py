e5340700424a7322f55b2e1c541f3c9f126fa3c0
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-01 19:34:54 -0300

    - Criado if para colocar mais um número um caso o primeiro termo seja um

--- ex063.py
+++ ex063.py
@@ -20,6 +20,7 @@
 #   - Criada área para variáveis íniciais- Criado o prnit para exibir frase de destaque
 #   - Criado while para calcular a sequência de Fibonacci
 #   - Criado print para mostrar o ultimo termo
+#   - Criado if para colocar mais um número um caso o primeiro termo seja um
 #
 # Licença: MIT.
 #
@@ -41,6 +42,11 @@ SomaDois = Inicio
 ###
 # Exibe a frase para destaque.
 print('Sequência de Fibonacci do termo {} e mais {} termos.'.format(Numero, NTermos + 1))
+###
+# If para colocar mais um número um.
+if Numero == 1:
+    print('1', end=', ')
+    NTermos = NTermos - 1
 ###
 # While para calcular e exibir a sequência de Fibonacci.
 while UltimoTermo != NTermos:
