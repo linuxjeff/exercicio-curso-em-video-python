a3458f25560d0f4a986d81d31e3a4dec3edbe140
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-01 19:58:59 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex064.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#
+# ex064 - Recebe vários numeros
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe vários números até receber o número
+#   novecentos e noventa e nove, depois mostra o total de números dititado
+#   e soma todos os número digitados.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 01-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
