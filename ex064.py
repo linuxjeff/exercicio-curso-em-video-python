b1ee0ada7114872878f4155d2c8afe750026534b
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-04 12:26:35 -0300

    - Criada área de variáveis iniciais - Criado while para receber números - Crado print para mostrar o resultado

--- ex064.py
+++ ex064.py
@@ -18,6 +18,28 @@
 #  v0.0.1 01-11-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada área de variáveis iniciais
+#   - Criado while para receber números
+#   - Crado print para mostrar o resultado
 #
 # Licença: MIT.
 #
+
+# Variáveis íniciais.
+
+Numero = int(0)
+
+Contador = int(0)
+
+Total = int(0)
+# Variáveis íniciais. ##
+# While para receber os números.
+while Numero != 999:
+    Numero = int(input('Para parar digite: 999\nDigite um número.\n>>> '))
+    if Numero != 999:
+        Contador += 1
+        Total = Total + Numero
+# While para receber os números. ##
+# Printe do resultado.
+print('Você digitou {} números. A soma de todos eles é: {}'.format(Contador, Total))
+# Printe do resultado. ##
