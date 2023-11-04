5c48660065b8fc851d2aee7f3028aa808a8b19de
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-04 12:53:55 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex065.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# ex064 - Recebe varios números.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe vários números até que o usuário escolha não digitar mais
+#   um número. Depois mostra a média, o número maior e o número menor.
+#
+# -----------------------------------------------------------------------------
+#
+#
+# Histórico:
+#
+#  v0.0.1 04-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
