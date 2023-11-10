e835004c7f514aef216cd93f20803a49633cbf63
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-10 13:24:54 -0300

    - Criada área de variáveis iniciais - Criado while para receber e somar os números - Criado o print para monstrar o resultado

--- ex066.py
+++ ex066.py
@@ -14,6 +14,26 @@
 #  v0.0.1 10-11-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada área de variáveis iniciais
+#   - Criado while para receber e somar os números
+#   - Criado o print para monstrar o resultado
 #
 # Licença: MIT.
 #
+
+# Variáveis iniciais.
+
+Soma = Contador = int(0)
+
+###
+# While para receber e somar número
+while True:
+    Numero = int(input('Digite um número.\nPara sair do programa digite: 999\n>>> '))
+    if Numero == 999:
+        break
+    Soma += Numero
+    Contador += 1
+###
+# Print para mostrar o resultado.
+print(f'Você digitou {Contador} números e a soma  deles é: {Soma}')
+###
