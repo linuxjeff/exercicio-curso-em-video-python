79a8c33479fcc56cbb65e8d04d3db2be71aaed27
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-10 13:44:19 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex067.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex067 - Mostra a tabuada.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa mostra a tabuada de um número digitado. O programa é
+#   encerrado quando for digitado um número negadivo.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 10-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
