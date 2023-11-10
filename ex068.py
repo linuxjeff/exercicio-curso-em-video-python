94c7123203c963670218289e748765baf1e531d6
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-10 18:37:44 -0300

    - Versão inicial #   - Criado o cabeçalho do programa

---
+++ ex068.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex063 - Jogo de par ou irpar.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Você joga par ou irpar contra a CPU. Ao perder é mostrado sua
+#   pontuação.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 10-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
