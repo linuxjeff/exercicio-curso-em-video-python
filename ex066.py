4b856a690d2e13fc1845fea2080499cc366db6b8
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-10 13:14:19 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex066.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#
+# ex066 - recebe números até ser digitado a flag.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe os números até ser digitado o número flag.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 10-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
