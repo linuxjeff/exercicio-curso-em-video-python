bcc90c7ee59ad3385573305d66a91b5d6bb3cd88
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-12 09:14:31 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex069.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex069 - Cadastro de pessoas
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa cadastra pessoas por idade e sexo.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 10-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
