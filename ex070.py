abbecf08f6e226904fb1e6b600959035314194f9
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-13 18:58:53 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex070.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# ex070 - Calculadora de compra
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe nome e valor de vários produtos e calcular o
+#   total da compra. Também mostra os produtos acima de mil reais e o produto
+#   mais barato.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 13-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
