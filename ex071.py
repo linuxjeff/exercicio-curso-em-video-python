f2feefb77ccf3bf22cdfb5833c74d094cb6b6dbd
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-15 07:08:10 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex071.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex071 - Calcula quantas notas seram entregue.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa simula um caixa eletronico
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 13-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
