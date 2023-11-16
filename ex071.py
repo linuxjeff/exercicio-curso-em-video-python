cf0acb5e2d661f9ad193bca6a3451c6760dd8b08
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-16 18:41:04 -0300

    - While foi modificado para só mostrar resultado quando tiver cedulas de um determinado valor - Retirado o print para mostrar o resultado do final

--- ex071.py
+++ ex071.py
@@ -17,6 +17,8 @@
 #   - Criada área das variáveis inicias
 #   - Criado while para cácular o números de cedulas
 #   - Criado print para mostrar o resultado
+#   - While foi modificado para só mostrar resultado quando tiver cedulas de um determinado valor
+#   - Retirado o print para mostrar o resultado do final
 #
 # Licença: MIT.
 #
@@ -33,23 +35,25 @@ while True:
         Mod50 = Valor // 50
         Valor = Valor - (Mod50 * 50)
         ControleDeCedula = 20
-    elif ControleDeCedula == 20:
+        if Mod50 > 0:
+            print(f'Total de cédula(s) de 50 reias: {Mod50}')
+    if ControleDeCedula == 20:
         Mod20 = Valor // 20
         Valor = Valor - (Mod20 * 20)
         ControleDeCedula = 10
-    elif ControleDeCedula == 10:
+        if Mod20 > 0:
+            print(f'Total de cédula(s) de 20 reias: {Mod20}')
+    if ControleDeCedula == 10:
         Mod10 = Valor // 10
         Valor = Valor - (Mod10 * 10)
         ControleDeCedula = 1
-    elif ControleDeCedula == 1:
+        if Mod10 > 0:
+            print(f'Total de cédula(s) de 10 reias: {Mod10}')
+    if ControleDeCedula == 1:
         Mod1 = Valor // 1
         Valor = Valor - (Mod1 * 1)
+        if Mod1 > 0:
+            print(f'Total de cédula(s) de um real: {Mod1}')
         break
     ###
-###
-# Mostrando resultado.
-print(f'Total de cédula(s) de 50 reias: {Mod50}'
-      f'\nTotal de cédula(s) de 20 reias: {Mod20}'
-      f'\nTotal de cédula(s) de 10 reias: {Mod10}'
-      f'\nTotal de cédula(s) de um real: {Mod1}')
 ###
