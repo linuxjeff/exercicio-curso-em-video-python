54bac140dffc30ae99639feec2a1740953e0717a
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-20 19:04:40 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex072.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex072 - Mostra nome do número por extenso.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe um número pelo teclado e mostra seu nome por extenso.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 20-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
