389dba927e8e40c7a7c3a0a9a3108993536b1cea
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-26 09:17:45 -0300

    - Versão inicial - Criado cabeçalho do programa

---
+++ ex073.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex072 - Mostra algumas coisas da tabela do brasileirão
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa mostra algumas curiosidade da tabela do brasileirão.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 26-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho do programa
+#
+# Licença: MIT.
+#
