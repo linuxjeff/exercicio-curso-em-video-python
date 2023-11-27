76ad5a40c13d34212d471a6fb9774cbd9ec34328
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-27 19:49:05 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex075.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# ex075 - Recebe quatro números.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe quatro números e diz quantas vezes o nove apareceu.
+#   Diz em qual posição pareceu o primeiro três. Mostra quais form o número
+#   pares.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 27-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
