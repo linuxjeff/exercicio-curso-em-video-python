8c585b60335056b8d0904626ad50ac9fabc135fa
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-11-28 20:12:02 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex076.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex076 - Lista de produtos
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa mostra uma lista de produtos gerados por uma tupla.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 28-11-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
