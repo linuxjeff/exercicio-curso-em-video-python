c254dfe3695ce5c73f680b6fba33792c7bde69f8
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-01 20:17:44 -0300

    - Versão inicial - Criado cabeçalho do programa

---
+++ ex077.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex077 - Mostra as vogais das palavras
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe palavras de uma tupla e mostra todas as vogais.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 01-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho do programa
+#
+# Licença: MIT.
+#
