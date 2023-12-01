17c5944dd6b91572536d66d08923b1fd6fde7b58
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-01 20:05:12 -0300

    - Criada área das variáveis inicias - Criado o código para criar a tabulação dos produtos com seus valores

--- ex076.py
+++ ex076.py
@@ -14,6 +14,23 @@
 #  v0.0.1 28-11-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada área das variáveis inicias
+#   - Criado o código para criar a tabulação dos produtos com seus valores
 #
 # Licença: MIT.
 #
+# Variáveis iniciais.
+Lista = ('Ovo', 0.20, 'Marcarão', 2, 'Gilo', 2.99, 'Sabão', 1.99, 'Óleo de Soja', 5)
+Nome = str('Lista de Produtos')
+Anderlane = '_' * 40
+###
+# Mostrando lista na tela.
+print(f'{Anderlane}')
+print(f'{Nome:^40}')
+print(f'{Anderlane}')
+Preco = int(1)
+for Produto in Lista[0::2]:
+    print(f'{Produto:•<30} R${Lista[Preco]:> .2f}')
+    Preco += 2
+print(f'{Anderlane}')
+###
