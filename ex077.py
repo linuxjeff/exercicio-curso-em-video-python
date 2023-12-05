51e0e0560cef104e26d93742f8537e43c75aad2b
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-05 18:31:46 -0300

    - Criada área das variáveis iniciais - Criado código para pegar e mostrar a vogais das palavras

--- ex077.py
+++ ex077.py
@@ -14,6 +14,21 @@
 #  v0.0.1 01-12-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado cabeçalho do programa
+#   - Criada área das variáveis iniciais
+#   - Criado código para pegar e mostrar a vogais das palavras
 #
 # Licença: MIT.
 #
+# Variáveis iniciais
+ListaDePalavras = ('Carro', 'Banana', 'Nokia', 'show', 'Angelica', 'Elemento')
+###
+# Criado código para pegar e mostrar cada vogal das palavras.
+print('Quais são as vogais?')
+for Palavra in ListaDePalavras:
+    Vogais = str()
+    print(f'A vogais da palavra {Palavra.upper()} são:', end=' ')
+    for Letras in Palavra:
+        if Letras.lower() == 'a' or Letras.lower() == 'e' or Letras.lower() == 'i' or Letras.lower() == 'o' or Letras.lower() == 'u':
+            Vogais += Letras.lower() + ' '
+    print(Vogais)
+###
