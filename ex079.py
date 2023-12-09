20f600bc820cc75003a17b729a5f2f44e5cf20b1
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-09 18:05:53 -0300

    - Versão inicial - Criado cabeçalho do programa

---
+++ ex079.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex079 - Recebe vários números sem duplicada.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe números pelo teclado sem aceitar números
+#   duplicados e mostra valores em ordem crecente no final.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 09-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho do programa
+#
+# Licença: MIT.
+#
