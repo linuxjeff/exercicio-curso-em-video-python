99471b9285b49400c0af920d19e96bf24eaca364
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-09 13:56:28 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex078.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex078 - Recebe cinco números pelo teclado
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: programa recebe cinco número pelo teclado e analiza o maior
+#   e o menor e mostra em qual posição foram colocados.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 09-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
