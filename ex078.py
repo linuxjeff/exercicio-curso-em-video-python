bf0585205b0ca45ef925f6e9113e6d7353ad96a5
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-09 14:48:12 -0300

    - Criado código para mostrar números digitados

--- ex078.py
+++ ex078.py
@@ -19,6 +19,7 @@
 #   - Criado código para receber cinco números pelo teclado
 #   - Criado código para mostrar maior e menor números e suas posições
 #   - Melhoria para mostrar a posição que o número vai oculpar
+#   - Criado código para mostrar números digitados
 #
 # Licença: MIT.
 #
@@ -31,10 +32,15 @@ for Contador in range(0, 5):
     Numeros.append(int(input(f'Digite um número {RealPosicao}: ')))
     RealPosicao += 1
 ###
+# Código para mostrar os números digitados
+print(f'Você digitou os números: ', end='')
+for EmNumeros in Numeros:
+    print(EmNumeros, end=' ')
+###
 # Código para mostrar o maior e menor números e suas posições.
 Maior = max(Numeros)
 Menor = min(Numeros)
-print(f'Maior número é {Maior}, ele esta na(s) Posição(ães): ', end='')
+print(f'\nMaior número é {Maior}, ele esta na(s) Posição(ães): ', end='')
 for Posicao, Numero in enumerate(Numeros):
     if Numero == Maior:
         print(Posicao + 1, end=' ')
