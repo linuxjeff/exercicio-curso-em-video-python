224215db27d439aaabd51c438d56eea9efb9cd15
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-10 10:55:32 -0300

    - Criada área de variáveis iniciais - Criado código para receber valores e não aceitar duplicadas - Criado códigos para mostrar valores em ordem crescente

--- ex079.py
+++ ex079.py
@@ -15,6 +15,37 @@
 #  v0.0.1 09-12-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado cabeçalho do programa
+#   - Criada área de variáveis iniciais
+#   - Criado código para receber valores e não aceitar duplicadas
+#   - Criado códigos para mostrar valores em ordem crescente
 #
 # Licença: MIT.
 #
+# Variáveis iniciais
+Numeros = list()
+###
+# Código para receber os números.
+Casou = int(0)
+Sair = str('A')
+while Sair[0] != 'N':
+    NovoNumero = int(input('Digite um número: '))
+    for Duplicado in Numeros:
+        if Duplicado == NovoNumero:
+            Casou = Duplicado
+    if Casou == NovoNumero:
+        print(f'O número {Casou} já existe. Tente outro.')
+    else:
+        Numeros.append(NovoNumero)
+    while True:
+        Sair = str(input('Deseja continuar? [S/N]\n>>> ')).strip().upper()
+        if Sair[0] in 'NS':
+            break
+        else:
+            print('Opçãp invalida! Tente novamente.')
+###
+# Código para mostrar os valores digitados em odem
+print('Você digitou os sequinte números: ', end='')
+Numeros.sort()
+for Valores in Numeros:
+    print(Valores, end=' ')
+###
