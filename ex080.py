a699fe7569332f0cccab80fa2e73ec60483f8473
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-11 12:49:27 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex080.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex080 - Recebe cinco números e ordena eles na lista.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe cinco números pelo teclado e ordena eles
+#   na lista sem usar a função sort.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 11-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
