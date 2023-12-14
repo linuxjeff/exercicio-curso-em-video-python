ced814de0300e4fca5d75f35acae2462e8c64d08
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-14 13:41:36 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex081.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# ex081 - Recebe vários números
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe vários números e mostra quantos números foram
+#   digitados, mostra os números em ordem decrescente e mostra se o número
+#   cinco foi digitado.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 14-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
