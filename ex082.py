f2a64023ec7b872f87e2b80af4a4028a8503a6b0
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-16 14:22:38 -0300

    - Versão inicial - Criado cabeçalho do programa

---
+++ ex082.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# nome_completo - Recebe números e separa em pares e impares
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe quantos números forem digitados e separa em duas listas,
+#   uma lista dos números pares e outra lista dos números impares
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 16-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho do programa
+#
+# Licença: MIT.
+#
