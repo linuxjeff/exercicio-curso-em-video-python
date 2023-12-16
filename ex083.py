529f83d8c7b2e768748615c2107eaf36931b3d96
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-16 15:11:58 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex083.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# nome_completo - Recebe e analisa uma expressão regular.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe uma expressão regular e analiza se ela esta correta.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 16-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
