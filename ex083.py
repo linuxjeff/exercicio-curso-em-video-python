622411658f8b904503f3c8913cd4f7b9d9471181
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-17 08:31:14 -0300

    - Criada a área de variáveis iniciais - Criado código para contar os parênteses e mostrar o resultado.

--- ex083.py
+++ ex083.py
@@ -14,6 +14,22 @@
 #  v0.0.1 16-12-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de variáveis iniciais
+#   - Criado código para contar os parênteses e mostrar o resultado.
 #
 # Licença: MIT.
 #
+# Variaveis inicias
+Parenteses = list(str(input('Digite uma expressão: ')))
+###
+# Código para verificar a quantidade de parênteses e mostra o resultado.
+Contador = int(0)
+for Caracteres in Parenteses:
+    if Caracteres == '(' or Caracteres == ')':
+        Contador += 1
+if Contador % 2 == 0:
+    print('A expressão esta correta!')
+else:
+    print('A expressão esta incorreta!')
+
+###
