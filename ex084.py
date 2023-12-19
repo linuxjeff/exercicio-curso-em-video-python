f7c4cc7cf5f4de9e7cae990f6c16fba7deaa5f87
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-19 13:42:51 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex084.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex084 - Recebe nome e peso das pessoas
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe nome e peso de várias pessoas e mostra quantas
+#   pessoas foram cadastradas os mais pesados e os menos pesados.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 19-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
