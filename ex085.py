23c9bdb5910244ee98578ab4dbb03816c30f0a3d
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-23 14:30:17 -0300

    - Criada a área de variáveis iniciais - Criado o código que recebe os números e separa na lista Pares e Impares e depois coloca tudo na lista Numeros - Criado o código que mostra os números pares - Criado o código que mostra os números impares

--- ex085.py
+++ ex085.py
@@ -15,6 +15,57 @@
 #  v0.0.1 23-12-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de variáveis iniciais
+#   - Criado o código que recebe os números e separa na lista Pares e Impares e depois coloca tudo na lista Numeros
+#   - Criado o código que mostra os números pares
+#   - Criado o código que mostra os números impares
 #
 # Licença: MIT.
 #
+# Variáveis iniciais.
+Numeros = list()
+
+Pares = list()
+
+Impares = list()
+###
+# Código recebe números e coloca nas listas Pares e Impares. Depois coloca as duas listas na lista Numeros.
+for Numero in range(1, 8):
+    Valor = int(input(f'Digite o {Numero}º número: '))
+    if Valor % 2 == 0:
+        Pares.append(Valor)
+        Pares.sort()
+    else:
+        Impares.append(Valor)
+        Impares.sort()
+    if Numero == 7:
+        Numeros.append(Pares[:])
+        Numeros.append(Impares[:])
+##
+# Separação.
+print('_-' * 30)
+###
+# Código para mostrar números impares.
+print('Números pares: ', end='')
+Par = Numeros[0]
+LenPares = len(Par)
+for Posicao, NuPar in enumerate(Par):
+    if LenPares == 1:
+        print(NuPar)
+    elif LenPares != Posicao + 1:
+        print(NuPar, end=', ')
+    else:
+        print(NuPar)
+###
+# Código para mostrar números impares.
+print('Números impares: ', end='')
+Impar = Numeros[1]
+LenImpares = len(Impar)
+for Posicao, NuImpar in enumerate(Impar):
+    if LenImpares == 1:
+        print(NuImpar)
+    elif LenImpares != Posicao + 1:
+        print(NuImpar, end=', ')
+    else:
+        print(NuImpar)
+###
