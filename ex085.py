39890e3de74f19255c8262cb4d5088e235f1f44a
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-23 13:05:29 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex085.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex085 - Programa recebe sete números.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe sete numeros e separa em duas listas de
+#   numeros pares e impares.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 23-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
