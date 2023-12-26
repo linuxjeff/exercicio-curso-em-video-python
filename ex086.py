9ee1239f833fd488d8c6f91cefa25cb4e620b063
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2023-12-26 13:50:13 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex086.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex086 - Cria uma matriz
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe valores do teclado e cria uma matriz dimencional
+#   de 3x3 na saída final do programa.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 26-12-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+# Licença: MIT.
+#
