f6c7ee8c41da91de1b10765e55a5efa2d6f2209e
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-11 12:32:59 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex088.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# nome_completo - Gera palpites da megasena
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa gera quantos palpites da mega sena for desejados.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 11-01-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
