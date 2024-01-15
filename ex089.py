bdd021e71da531673b5999c8272aefe8d69bb637
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-15 13:50:54 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex089.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# nome_completo - Guarda as duas notas dos alunos.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Guarda as duas notas dos alunos e calcula a média.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 15-01-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
