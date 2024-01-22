c140499dec5771d682e606b28b877f28fdc6c004
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-22 14:34:54 -0300

    - Criado código que recebe os alunos e suas notas

--- ex089.py
+++ ex089.py
@@ -14,6 +14,33 @@
 #  v0.0.1 15-01-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criado código que recebe os alunos e suas notas
 #
 # Licença: MIT.
 #
+# Variáveis iniciais
+Dados = list()
+###
+# Código que recebe o cadastro dos alunos e suas notas e já cacula a média.
+while True:
+    FimCadastro = int(0)
+    Nome = input('Nome do aluno: ')
+    NotaUm = float(input('Primenira nota: '))
+    NotaDois = float(input('Segunda nota: '))
+    Media = (NotaUm + NotaDois) / 2
+    Dados.append([Nome, [NotaUm, NotaDois, Media]])
+    while True:
+        Proximo = str(input('Deseja continuar? (S|N)\n>>> ')).upper()
+        if Proximo[0] == 'N':
+            FimCadastro = 1
+            print('Fim do cadastro!')
+            break
+        elif Proximo[0] == 'S':
+            print('Próximo Aluno!')
+            break
+        else:
+            print('Opção invalida! Tente novamente.')
+    if FimCadastro == 1:
+        break
+
+print(Dados)
