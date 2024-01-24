acad15eb2507312f3fa4ac801be3f4a913f26da2
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-24 18:49:20 -0300

    - Criado código que mostra os alunos e suas médias - Criado código para mostrar notas de cada aluno por escolha do usuário

--- ex089.py
+++ ex089.py
@@ -15,6 +15,8 @@
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
 #   - Criado código que recebe os alunos e suas notas
+#   - Criado código que mostra os alunos e suas médias
+#   - Criado código para mostrar notas de cada aluno por escolha do usuário
 #
 # Licença: MIT.
 #
@@ -24,7 +26,7 @@ Dados = list()
 # Código que recebe o cadastro dos alunos e suas notas e já cacula a média.
 while True:
     FimCadastro = int(0)
-    Nome = input('Nome do aluno: ')
+    Nome = input('Nome do aluno: ').capitalize()
     NotaUm = float(input('Primenira nota: '))
     NotaDois = float(input('Segunda nota: '))
     Media = (NotaUm + NotaDois) / 2
@@ -43,4 +45,28 @@ while True:
     if FimCadastro == 1:
         break
 ###
-print(Dados)
+# Código mostra os alunos e suas médias.
+print('N° Nome            Média')
+for Posicao, Aluno in enumerate(Dados):
+    print(f'{Posicao + 1}  {Dados[Posicao][0]:<18}{Dados[Posicao][1][2]:.1f}')
+###
+# Código para mostrar notas de cada aluno por escolha por escolha do usuário.
+Inicio = int(0)
+while True:
+    if Inicio == 1:
+        print('N° Nome            Média')
+        for Posicao, Aluno in enumerate(Dados):
+            print(f'{Posicao + 1}  {Dados[Posicao][0]:<18}{Dados[Posicao][1][2]:.1f}')
+        AlunaN = int(input('Digite o número do aluno: '))
+        print(f'{Dados[AlunaN - 1][0]:<18}{Dados[AlunaN - 1][1][0]:.1f}, {Dados[AlunaN - 1][1][1]:.1f}')
+    elif Inicio == 2:
+        break
+    while True:
+        VerNotas = str(input('Deseja ver a nota de algum aluno? [S|N]\n>>> ')).upper()
+        if VerNotas[0] == 'N':
+            Inicio = 2
+            break
+        elif VerNotas[0] == 'S':
+            Inicio = 1
+            break
+###
