9d0a706b047f1ff59ab6ba47f9dfcfa604d2310b
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-31 13:03:23 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex091.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex091 - Sortea o dado para quatro jogadores.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa sortea o número de um a seis para quatro jogadore e
+#   depois coloca em ordem.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 xx-xx-xxxx, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
