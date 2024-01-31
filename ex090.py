ee483452889039de065c62cd83e757039fb315f4
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-31 12:53:04 -0300

    - Criada a área das variáveis iniciais - Criado o código para receber nome e média e mostrar a situação final do aluno

--- ex090.py
+++ ex090.py
@@ -15,6 +15,25 @@
 #  v0.0.1 xx-xx-xxxx, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área das variáveis iniciais
+#   - Criado o código para receber nome e média e mostrar a situação final do aluno
 #
 # Licença: MIT.
 #
+# Variáveis iniciais.
+aluno = dict()
+###
+# Código para receber nome e média e mostrar a situação final do aluno.
+aluno['nome'] = str(input('Digite o nome do aluno: ')).capitalize()
+
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+
+print(f'Nome é igual a {aluno["nome"]}')
+
+print(f'Média é igual a {aluno["media"]}')
+
+if aluno['media'] > 5:
+    print('Situação igual a Aprovado')
+else:
+    print('Situação é igual a Reprovado')
+###
