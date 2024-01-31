1cef9bdd4ff543c3144f826c3dbd73b3fcc47a63
Jefferson Vitor de Santana <computadores.rp@gmail.com>
2024-01-31 12:31:16 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex090.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex090 - Recebe a média de um aluno
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe a média de um aluno e mostra na tela sua
+#   situação.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 xx-xx-xxxx, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
