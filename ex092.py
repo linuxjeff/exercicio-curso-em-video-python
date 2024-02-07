ca423df86368e353550a0492577e3e1dacf4fdae
jefferson <jeff92235392@gmail.com>
2024-02-07 12:58:53 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex092.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex092 - Lê nome, ano de nascimento e ctps.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe o nome, ano de nascimento e CTPS. Caso tenha
+#   CTPS pede o ano de contratação e mostra o resultado na tela.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 07-02-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
