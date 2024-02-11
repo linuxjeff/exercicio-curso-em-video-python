21e1fa211d1d2155ce83b63b69e43e9c309fef8c
jefferson <jeff92235392@gmail.com>
2024-02-10 21:47:24 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex093.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex093 - Cadastra o aproventamento de um jogador
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe o números de jogos e os gols feitos nos jogos
+#   de um jogador. Depois mostra o aproveitamento.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 10-02-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
