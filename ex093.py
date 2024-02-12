e7093a16b28bc2d36c6d3bce658e885b162564a5
jefferson <jeff92235392@gmail.com>
2024-02-12 13:04:50 -0300

    - Criada a área de variáveis iniciais - Criado o código para receber os dados

--- ex093.py
+++ ex093.py
@@ -15,6 +15,24 @@
 #  v0.0.1 10-02-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de variáveis iniciais
+#   - Criado o código para receber os dados
 #
 # Licença: MIT.
 #
+# Variáveis iniciais
+Jogador = dict()
+Gols = list()
+###
+# Código para receber os dados.
+Jogador['Nome'] = str(input('Nome do jogador: ')).strip().capitalize()
+Total = int(0)
+Jogos = int(input('Quantos Jogos ele jogou?\n>>> '))
+for Contador in range(0, Jogos):
+    NumeroDeGols = int(input(f'Quantos gols na partida {Contador + 1}: '))
+    Total += NumeroDeGols
+    Gols.append(NumeroDeGols)
+Jogador['Gols'] = Gols
+Jogador['Total'] = Total
+###
+print(Jogador, Jogos, Gols, Total)
