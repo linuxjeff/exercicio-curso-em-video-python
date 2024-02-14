3af97b89fdfb4f4fc55141e69a7587c04aab4d4a
jefferson <jeff92235392@gmail.com>
2024-02-14 12:47:51 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex094.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex094 - Cadastra varias pessoas com sexo e idade.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa cadastra pessoas com o sexo e a idade. Depois mostra
+#   um relatórios dos dados.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 14-02-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
