1a97903efa8687d66f85273065bb27174b394b49
jefferson <jeff92235392@gmail.com>
2024-02-18 10:01:29 -0300

    - Criada a área de variáveis iniciais - Criado o código que recebe o cadastro

--- ex094.py
+++ ex094.py
@@ -15,6 +15,49 @@
 #  v0.0.1 14-02-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de variáveis iniciais
+#   - Criado o código que recebe o cadastro
 #
 # Licença: MIT.
 #
+# Variáveis iniciais.
+
+CadastroDePessoas = list()
+
+Mulheres = list()
+
+Pessoa = dict()
+
+Idade = TotalIdade = TotalGrupo = FimDoCadastro = int(0)
+###
+# Código que recebe o cadastro.
+while True:
+    Pessoa['Nome'] = str(input('Nome: ')).strip().capitalize()
+    while True:
+        Sexo = str(input('Sexo: [M/F] ')).strip().upper()
+        if Sexo[0] == 'M' or Sexo[0] == 'F':
+            Pessoa['Sexo'] = Sexo[0]
+            break
+        else:
+            print('Opção invalida! Tente novamente.')
+    if Pessoa['Sexo'] == 'F':
+        Mulheres.append(Pessoa['Nome'])
+    Idade = int(input('Idade: '))
+    Pessoa['Idade'] = Idade
+    TotalIdade += Idade
+    TotalGrupo += 1
+    CadastroDePessoas.append(Pessoa.copy())
+    Pessoa.clear()
+    while True:
+        QuerContinuar = str(input('Quer Continuar? [S|N]\n>>> ')).strip().upper()
+        if QuerContinuar[0] == 'N':
+            FimDoCadastro = 1
+            break
+        elif QuerContinuar[0] == 'S':
+            break
+        else:
+            print('Opção invalida! Tente novamnete.')
+    if FimDoCadastro == 1:
+        break
+###
+print(Mulheres, Pessoa, TotalIdade, TotalGrupo, CadastroDePessoas)
