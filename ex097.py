f9531aede2a2e617ce67df504e9d0f20d163b37d
jefferson <jeff92235392@gmail.com>
2024-03-07 13:35:49 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex097.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex097 - Programa recebe frases e faz um decoração
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa rece uma frase e fas uma decoração com duas linhas
+#   uma emcima e outra embaixo.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 08-03-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
