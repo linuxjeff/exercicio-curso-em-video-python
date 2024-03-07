3a64562a7f447eab2ec2b016e68a98db02d5dbf8
jefferson <jeff92235392@gmail.com>
2024-03-07 13:00:40 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex096.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex096 - Calcula a área do terrono.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Este programa calcula a área do terreno.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 07-03-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
