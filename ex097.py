1809b8e25626aff93795543659af9c4673f7f6ef
jefferson <jeff92235392@gmail.com>
2024-03-11 12:30:16 -0300

    - Criado a def que cria o título decorado - Criada área de exemplo

--- ex097.py
+++ ex097.py
@@ -15,6 +15,24 @@
 #  v0.0.1 08-03-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criado a def que cria o título decorado
+#   - Criada área de exemplo
 #
 # Licença: MIT.
 #
+# Declarando defs.
+def MostreaLinha(msg):
+    tamanholen = len(msg) + 6
+    print('-' * tamanholen)
+    print(f'{msg:^{tamanholen}}')
+    print('-' * tamanholen)
+
+
+###
+# Aŕea de exemplos
+MostreaLinha('Gustavo Guanabara')
+
+MostreaLinha('Curso de Python no YouTube')
+
+MostreaLinha('CeV')
+###
