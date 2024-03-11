bc01630cdf198c8130be6550a0a925561a280d9f
jefferson <jeff92235392@gmail.com>
2024-03-11 12:37:58 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex098.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex098 - Contador personalizado
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe três parametros e gera uma contagem aparti
+#   dele.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 11-03-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
