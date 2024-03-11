9a4850198bddf2a9bdd9db4111b91a36b62823f2
jefferson <jeff92235392@gmail.com>
2024-03-11 13:39:36 -0300

    - Criada a área de importação de bibliotecas - Criada a área das defs - Criada a área de exemplos - Criada o código para receber e mostrar a contagem

--- ex098.py
+++ ex098.py
@@ -15,6 +15,48 @@
 #  v0.0.1 11-03-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de importação de bibliotecas
+#   - Criada a área das defs
+#   - Criada a área de exemplos
+#   - Criada o código para receber e mostrar a contagem
+#
 #
 # Licença: MIT.
 #
+# Importado bibliotecas
+from time import sleep
+
+
+# Defs
+
+def contador(inicio, fim, passo):
+    print('-_' * 30)
+    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
+    if fim <= 0:
+        fim = fim + -1
+    elif fim <= inicio:
+        fim = fim - 1
+    else:
+        fim = fim + 1
+    if fim < inicio:
+        passo = passo - (passo * 2)
+    for numero in range(inicio, fim, passo):
+        sleep(0.5)
+        print(numero, end=' ')
+    print('FIM')
+
+
+###
+# Área de exemplos.
+contador(0, 10, 1)
+
+contador(10, 0, 2)
+###
+# Código para receber e mostrar a contagem.
+print('-_' * 30)
+Inicio = int(input('Inicío: '))
+Fim = int(input('Fim: '))
+Passo = int(input('Passo: '))
+
+contador(Inicio, Fim, Passo)
+###
