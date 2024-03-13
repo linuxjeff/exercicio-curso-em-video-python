87c25ad196b01305967b2f49a3c758e1c1504858
jefferson <jeff92235392@gmail.com>
2024-03-13 12:59:36 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex099.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex096 - Analisa qual o maior número através da def.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Recebe números pelo teclado e analisa qual o maior número
+#   através da def.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 13-03-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
