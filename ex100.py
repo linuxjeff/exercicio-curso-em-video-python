a2dbe1d107bfc30cb1eaef3246ae13b3c462fb01
jefferson <jeff92235392@gmail.com>
2024-03-15 13:06:23 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex100.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex100 - Sortea vários valores e soma os pares.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa tedm duas defs uma faz o sorteio dos valores e a
+#   outra soma somente os números pares.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 15-03-2023, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
