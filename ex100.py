4134b4b72bcbddea5c93ea82063c5cff1249d2a5
jefferson <jeff92235392@gmail.com>
2024-03-18 12:47:14 -0300

    - Criada a área de importação de bibliotecas - Criada a área das DEFs - Criada a def sortea que sortea cinco números - Criada a def somapar que soma somente os numeros pares em uma lista - Criada a área das variáveis iniciais - Criado o código de execução principal

--- ex100.py
+++ ex100.py
@@ -15,6 +15,47 @@
 #  v0.0.1 15-03-2023, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de importação de bibliotecas
+#   - Criada a área das DEFs
+#   - Criada a def sortea que sortea cinco números
+#   - Criada a def somapar que soma somente os numeros pares em uma lista
+#   - Criada a área das variáveis iniciais
+#   - Criado o código de execução principal
 #
 # Licença: MIT.
 #
+# Importando bibliotecas.
+from random import randint
+from time import sleep
+
+
+###
+# Área das DEFs
+def sortea(lista):
+    print('Sorteando cinco valores da lista: ', end='')
+    for numero in range(1, 6):
+        paralista = randint(0, 10)
+        sleep(0.5)
+        if numero == 5:
+            print(f'{paralista} PRONTO!')
+        else:
+            print(f'{paralista}', end=' ')
+        lista.append(paralista)
+
+
+def somapar(lista):
+    somadospares = int(0)
+    for numero in lista:
+        if numero % 2 == 0:
+            somadospares += numero
+    print(f'Somando os números pares de {lista}, temos {somadospares}')
+
+
+###
+# Área de variáveis iniciais.
+NumerosAleatorios = list()
+###
+# Código de execução principal.
+sortea(NumerosAleatorios)
+somapar(NumerosAleatorios)
+###
