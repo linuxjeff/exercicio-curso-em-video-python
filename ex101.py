8a95c0d0c0b578a2f398b5441a40e66ee383714e
jefferson <jeff92235392@gmail.com>
2024-03-22 13:23:35 -0300

    - Criada a área de importa bibliotecas - Criada a área de funçãos - Criada a área das variáveis globais - Criada a área do código para exibir o resultado - Criada a função voto para mostrar se o voto é obrigatório - Criada a docstring da função voto

--- ex101.py
+++ ex101.py
@@ -15,6 +15,42 @@
 #  v0.0.1 22-03-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área de importa bibliotecas
+#   - Criada a área de funçãos
+#   - Criada a área das variáveis globais
+#   - Criada a área do código para exibir o resultado
+#   - Criada a função voto para mostrar se o voto é obrigatório
+#   - Criada a docstring da função voto
 #
 # Licença: MIT.
 #
+# Importando bibliotecas.
+from datetime import date
+
+
+###
+# Funções
+def voto(ano=date.today().year):
+    '''
+    A função voto mostra se  o voto é obrigatório ou facultativo.
+    :param ano: O ano de nascimento da pessoa
+    :return: retorna a contição do voto da pessoa
+    '''
+    idade = date.today().year - ano
+    resultado = str('')
+    if idade <= 15:
+        resultado = f'Com {idade} anos: NÃO VOTA'
+    elif idade < 18 or idade > 70:
+        resultado = f'Com {idade} anos: VOTO FACULTATIVO'
+    else:
+        resultado = f'Com {idade} anos: VOTO OBRIGATÓRIO'
+    return resultado
+
+
+###
+# Variáveis globais
+anodenascimento = int(input('Em que ano você nasceu?\n>>> '))
+###
+# Código para mostrar resultado.
+print(voto(anodenascimento))
+###
