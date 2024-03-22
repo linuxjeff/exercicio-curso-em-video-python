7a98ecc3740f627e4ac4c81ade93954139b5d769
jefferson <jeff92235392@gmail.com>
2024-03-22 13:41:57 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex102.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex102 - Calcula o fatorial
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe um número natural maior ou igual a dois e
+#   calcula seu fatorial.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 22-03-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
