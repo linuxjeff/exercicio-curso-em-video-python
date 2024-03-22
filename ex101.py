07f016eafd735dd7cba919ddfec8c65e4ac78620
jefferson <jeff92235392@gmail.com>
2024-03-22 12:36:48 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex101.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex101 - Recebe o ano de nascimento e retorna sobre o voto
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe o ano de nascimento e retona a contição atual
+#   do seu voto.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 22-03-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
