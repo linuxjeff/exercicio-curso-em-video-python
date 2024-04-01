121d7264eb53b22e55f45fab1f8b856cd52039bb
jefferson <jeff92235392@gmail.com>
2024-04-01 13:47:39 -0300

    - Criada a função ficha - Criada a área de variáveis globais - Criado o código que mostra o resultado(programa principal)

--- ex103.py
+++ ex103.py
@@ -15,6 +15,30 @@
 #  v0.0.1 01-04-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado cabeçalho do programa
+#   - Criada a função ficha
+#   - Criada a área de variáveis globais
+#   - Criado o código que mostra o resultado(programa principal)
 #
 # Licença: MIT.
 #
+# Funções
+import numbers
+
+
+def ficha(nome='desconhecido', gols='0'):
+    if nome == '' or nome == 'desconhecido':
+        nome = '<desconhecido>'
+    if gols == '' or gols == 0:
+        gols = 0
+    print(f'O jogador {nome.strip().capitalize()} fez {gols} gol(s) no campeonato.')
+
+
+###
+# Variávés globais
+Nome = str(input('Nome do jogador: '))
+
+Gols = input('Número de Gols: ')
+###
+# Código que mostra o resultado
+ficha(Nome, Gols)
+###
