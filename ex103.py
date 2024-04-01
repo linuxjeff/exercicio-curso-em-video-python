b46f12c911081f085d393148560eb63e49adcc28
jefferson <jeff92235392@gmail.com>
2024-04-01 13:13:45 -0300

    - Versão inicial - Criado cabeçalho do programa

---
+++ ex103.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex103 - Programa recebe nome e gols feito pelo jogador
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa recebe nome e gols feitos pelo jogador e depois mostra
+#   o resultado.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 01-04-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado cabeçalho do programa
+#
+# Licença: MIT.
+#
