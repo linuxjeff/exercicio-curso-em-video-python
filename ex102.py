888582e2a508d98df01d70d7c5d5e0e4dd59bbdc
jefferson <jeff92235392@gmail.com>
2024-04-01 13:07:37 -0300

    - Criado a função fatorial - Criado o código que mostra o resultado(Programa principal)

--- ex102.py
+++ ex102.py
@@ -15,6 +15,38 @@
 #  v0.0.1 22-03-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criado a função fatorial
+#   - Criado o código que mostra o resultado(Programa principal)
 #
 # Licença: MIT.
 #
+# Funções
+def fatorial(numero, show=False):
+    '''
+    fatorial(numero, show=False)
+    -> Calcula o fatorial de um número.
+    :param numero: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor  do Fatorial de um número n.
+    '''
+    anterior = numero
+    total = 0
+    while numero != 0:
+        if show:
+            if numero == 1:
+                print(f'{numero}', end='=')
+            else:
+                print(f'{numero}', end='x')
+        numero = numero - 1
+        anterior = anterior * numero
+        if numero == 1:
+            total = anterior
+
+    return total
+
+
+###
+# Código para mostrar o resultado
+print('-' * 30)
+print(fatorial(5, True))
+###
