07fb8b9f10c181db90c96f73edba91793d8c57a6
jefferson <jeff92235392@gmail.com>
2024-04-03 13:35:12 -0300

    - Criada a função leiaint que verifica se o conteudo é um número inteiro - Criado o código principal para mostrar o resultado

--- ex104.py
+++ ex104.py
@@ -14,6 +14,28 @@
 #  v0.0.1 03-04-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a função leiaint que verifica se o conteudo é um número inteiro
+#   - Criado o código principal para mostrar o resultado
 #
 # Licença: MIT.
 #
+# Funçãoes
+def leiaint(frase=''):
+    numero = str(input(f'{frase}')).upper().strip()
+    inteiro = numero.isnumeric()
+    while True:
+        if inteiro:
+            numero = int(numero)
+            break
+        else:
+            print('ERRO! Digite um número inteiro válido.')
+            numero = str(input(f'{frase}')).upper().strip()
+            inteiro = numero.isnumeric()
+    return numero
+
+
+###
+# Código principal
+Onumero = leiaint('Digite um número: ')
+print(f'Você acabou de digitar o número: {Onumero}')
+###
