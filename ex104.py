e3ce2d2a53105527ca1fa2f45a06fa4ae636e09e
jefferson <jeff92235392@gmail.com>
2024-04-03 12:35:00 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex104.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex104 - Recebe um número e verifica se é um número interiro
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Programa verifica se o valor digitado é um número inteiro.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 03-04-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
