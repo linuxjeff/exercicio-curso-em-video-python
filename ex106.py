2366bfeed1993997dd0d41046e30ed5f611aa03f
jefferson <jeff92235392@gmail.com>
2024-04-05 13:43:22 -0300

    - Criada a área para importar bibliotecas - Criada a função mostrealinha - Criado o códgo principal

--- ex106.py
+++ ex106.py
@@ -14,6 +14,32 @@
 #  v0.0.1 05-04-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criada a área para importar bibliotecas
+#   - Criada a função mostrealinha
+#   - Criado o códgo principal
 #
 # Licença: MIT.
 #
+# importando bibliotecas
+from time import sleep
+###
+# Funçoes
+def mostrealinha(msg):
+    tamanholen = len(msg) + 6
+    print('-' * tamanholen)
+    print(f'{msg:^{tamanholen}}')
+    print('-' * tamanholen)
+
+
+###
+# Código principal.
+while True:
+    mostrealinha('Sistema de ajuda PyHELP')
+    comando = str(input('Função ou Biblioteca > ')).strip().lower()
+    if comando == 'fim':
+        mostrealinha('Até Logo! :)')
+        break
+    mostrealinha(f'Acesando manual do comando {comando}')
+    help(comando)
+    sleep(10)
+###
