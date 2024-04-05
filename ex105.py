0b576976a6853db6daa3726df82a09b736c879b7
jefferson <jeff92235392@gmail.com>
2024-04-05 12:32:57 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex105.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex105 - Recebe várias notas
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe várias notas e mostra os dados relevantes.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 05-04-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
