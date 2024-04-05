4181aa811a27e3d7364d031d583717ebac32245f
jefferson <jeff92235392@gmail.com>
2024-04-05 13:26:49 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex106.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex106 - Mostra a ajuda interadiva
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Mostra a ajuda interadiva de cada programa.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 05-04-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
