dcf212504b4ea6fad1a508ebb9fff0d8a907d43f
jefferson <jeff92235392@gmail.com>
2024-04-12 13:33:34 -0300

    - Versão inicial - Criado o cabeçalho do módulo

---
+++ ex107.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex107 - Modulos -> aumentar(), diminuir(), dobro() e metade().
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Este módulo é voltado para calculor de moeda.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 12-04-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do módulo
+#
+# Licença: MIT.
+#
