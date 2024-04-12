76c579fdf2fce31a6d439c4a900eb065fc8ec6fc
jefferson <jeff92235392@gmail.com>
2024-04-12 13:38:56 -0300

    - Versão inicial - Criado o cabeçalho

---
+++ ex107-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex107-main - Programa para testar os módulos.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: Este programa é para testar os módulos do ex107.py.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 12-04-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho
+#
+# Licença: MIT.
+#
