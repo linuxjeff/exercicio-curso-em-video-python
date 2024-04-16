928a29e308d97041b86c68fd504600f790bc39d6
jefferson <jeff92235392@gmail.com>
2024-04-16 12:54:13 -0300

    - Criada a área de importação de bibliotecas - Criada a área da variáveis globais - Cridado o programa principal

--- ex107-main.py
+++ ex107-main.py
@@ -14,6 +14,21 @@
 #  v0.0.1 12-04-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho
+#   - Criada a área de importação de bibliotecas
+#   - Criada a área da variáveis globais
+#   - Cridado o programa principal
 #
 # Licença: MIT.
 #
+# Importando bibliotecas.
+import ex107
+###
+# Variáveis globais.
+preco = float(input('Digite um preço: R$'))
+###
+# Programa principal.
+print(f'A metade de R${preco} é {ex107.metade(preco)}')
+print(f'O dobro de R${preco} é {ex107.dobro(preco)}')
+print(f'Aumentando 10%, temos R${ex107.aumentar(preco, 10)}')
+print(f'Reduzindo 13%, temos R${ex107.diminuir(preco, 13)}')
+###
