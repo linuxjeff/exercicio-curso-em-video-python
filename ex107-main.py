a74eadecc2607fa70530f284fd13eb63147c4119
Jefferson <computadores.rp@gmail.com>
2024-04-16 20:26:45 -0300

    - Mudanças no programa principal para mostrar as formatações com a nova opção

--- ex107-main.py
+++ ex107-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #
-# ex108-main - Programa para testar os módulos.
+# ex109-main - Programa para testar os módulos.
 #
 # Site      : https://github.com/linuxjeff
 # Autor     : Jefferson Santana <computadores.rp@gmail.com>
 #
 # -----------------------------------------------------------------------------
-#   Descrição: Este programa é para testar os módulos do ex108.py.
+#   Descrição: Este programa é para testar os módulos do ex109.py.
 #
 # -----------------------------------------------------------------------------
 # Histórico:
@@ -20,19 +20,21 @@
 #  v0.0.2 16-04-2024, Jefferson Santana
 #   - Adicionado a função moeda para formatar os valores
 #  v0.0.3 16-04-2024, Jefferson Santana
-#   - Devido a mudança na biblioteca ex108 foram aterados linha correspondentes
+#   - Devido a mudança na biblioteca ex109 foram aterados linha correspondentes
+#  v0.0.4 19-04-2024, Jefferson Santana
+#   - Mudanças no programa principal para mostrar as formatações com a nova opção
 #
 # Licença: MIT.
 #
 # Importando bibliotecas.
-from ex108 import moeda
+from ex109 import moeda
 ###
 # Variáveis globais.
 preco = float(input('Digite um preço: R$'))
 ###
 # Programa principal.
-print(f'A metade de {moeda.moeda(preco)} é {moeda.moeda(moeda.metade(preco))}')
-print(f'O dobro de {moeda.moeda(preco)} é {moeda.moeda(moeda.dobro(preco))}')
-print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(preco, 10))}')
-print(f'Reduzindo 13%, temos {moeda.moeda(moeda.diminuir(preco, 13))}')
+print(f'A metade de {moeda.moeda(preco)} é {moeda.metade(preco, True)}')
+print(f'O dobro de {moeda.moeda(preco)} é {moeda.dobro(preco, True)}')
+print(f'Aumentando 10%, temos {moeda.aumentar(preco, 10, True)}')
+print(f'Reduzindo 13%, temos {moeda.diminuir(preco, 13)}')
 ###
