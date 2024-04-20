c10f567567c356a82b5ed3bc2f250277f6426305
jefferson <jeff92235392@gmail.com>
2024-04-20 18:12:23 -0300

    - Alterado programa principal para usar a def resumo

--- ex107-main.py
+++ ex107-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 #
-# ex109-main - Programa para testar os módulos.
+# ex110-main - Programa para testar os módulos.
 #
 # Site      : https://github.com/linuxjeff
 # Autor     : Jefferson Santana <computadores.rp@gmail.com>
 #
 # -----------------------------------------------------------------------------
-#   Descrição: Este programa é para testar os módulos do ex109.py.
+#   Descrição: Este programa é para testar os módulos do ex110.py.
 #
 # -----------------------------------------------------------------------------
 # Histórico:
@@ -20,21 +20,20 @@
 #  v0.0.2 16-04-2024, Jefferson Santana
 #   - Adicionado a função moeda para formatar os valores
 #  v0.0.3 16-04-2024, Jefferson Santana
-#   - Devido a mudança na biblioteca ex109 foram aterados linha correspondentes
+#   - Devido a mudança na biblioteca ex110 foram aterados linha correspondentes
 #  v0.0.4 19-04-2024, Jefferson Santana
 #   - Mudanças no programa principal para mostrar as formatações com a nova opção
+#  v0.0.5 20-04-2024, Jefferson Santana
+#   - Alterado programa principal para usar a def resumo
 #
 # Licença: MIT.
 #
 # Importando bibliotecas.
-from ex109 import moeda
+from ex110 import moeda
 ###
 # Variáveis globais.
 preco = float(input('Digite um preço: R$'))
 ###
 # Programa principal.
-print(f'A metade de {moeda.moeda(preco)} é {moeda.metade(preco, True)}')
-print(f'O dobro de {moeda.moeda(preco)} é {moeda.dobro(preco, True)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(preco, 10, True)}')
-print(f'Reduzindo 13%, temos {moeda.diminuir(preco, 13)}')
+moeda.resumo(preco, 80, 35)
 ###
