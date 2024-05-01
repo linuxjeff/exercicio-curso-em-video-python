2c63af340218b7b0a3172429b488d79ca3fa4adc
jefferson <jeff92235392@gmail.com>
2024-05-01 11:37:57 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex113.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex113 - O programa recebe dois valores.
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa recebe um valor inteiro e um valor flutuante.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 01-05-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
