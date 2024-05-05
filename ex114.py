d8697185d091553dd5d66a4059b927a5d83b308f
jefferson <jeff92235392@gmail.com>
2024-05-05 09:06:19 -0300

    - Criado a área para importar bibliotecas - Criado programa principal que testa se o site esta acessível

--- ex114.py
+++ ex114.py
@@ -14,6 +14,19 @@
 #  v0.0.1 05-05-2024, Jefferson Santana
 #   - Versão inicial
 #   - Criado o cabeçalho do programa
+#   - Criado a área para importar bibliotecas
+#   - Criado programa principal que testa se o site esta acessível
 #
 # Licença: MIT.
 #
+# Importando bibliotecas
+import requests
+
+###
+# Programa principal
+try:
+    resposta = requests.get('https://www.google.com.br')
+    print('\033[32mO site esta acessível no momento\033[m')
+except:
+    print('\033[31mO site não esta inacessível.\033[m')
+###
