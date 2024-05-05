32457f1174c7c32309d863e7034055e9f5ce0e14
jefferson <jeff92235392@gmail.com>
2024-05-05 08:37:59 -0300

    - Versão inicial - Criado o cabeçalho do programa

---
+++ ex114.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# ex114 - Teste se o site esta no ar
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa testa se um site esta funcionando.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 05-05-2024, Jefferson Santana
+#   - Versão inicial
+#   - Criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
