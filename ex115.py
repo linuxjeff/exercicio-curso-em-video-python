796812481aafe2a3a362fe5bc104fc73b3477c19
jefferson <jeff92235392@gmail.com>
2024-05-06 12:49:05 -0300

    - Versão inicial - Foi criado o cabeçalho do programa

---
+++ ex115.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#
+# ex115 - Sistema para cadastro
+#
+# Site      : https://github.com/linuxjeff
+# Autor     : Jefferson Santana <computadores.rp@gmail.com>
+#
+# -----------------------------------------------------------------------------
+#   Descrição: O programa cadastra pessoas e sua idade e quanda em um banco de
+#   dado do tipo txt.
+#
+# -----------------------------------------------------------------------------
+# Histórico:
+#
+#  v0.0.1 06-05-2024, Jefferson Santana
+#   - Versão inicial
+#   - Foi criado o cabeçalho do programa
+#
+# Licença: MIT.
+#
